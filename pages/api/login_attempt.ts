2b205087543a7ed513525ad88387abea2ceda470
Ed Parsons <ed@acutulus.co>
2022-05-27 21:18:09 -0700

    remove console

--- pages/api/login_attempt.ts
+++ pages/api/login_attempt.ts
@@ -5,8 +5,6 @@ import Airtable from 'airtable'
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== 'POST') return res.status(405).end()
   
-  console.log({body: req.body})
-
   var base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE || '');
   base('email_attempts').create([
     {
