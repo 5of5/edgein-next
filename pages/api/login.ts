5ef65eca96bc371116880901a38e2a15a67e2cee
Ed Parsons <ed@acutulus.co>
2022-06-12 22:01:09 -0700

    Adding some permissions

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -4,6 +4,16 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { nanoid } from 'nanoid'
 import { SignJWT } from 'jose'
 
+const hasuraClaims = {
+  "https://hasura.io/jwt/claims": {
+    "x-hasura-allowed-roles": ["user"],
+    "x-hasura-default-role": "user",
+    // "x-hasura-user-id": "1234567890",
+    // "x-hasura-org-id": "123",
+    // "x-hasura-custom": "custom-value"
+  }
+}
+
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== 'POST') return res.status(405).end()
 
@@ -13,11 +23,11 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const user = await new Magic(process.env.MAGIC_SECRET_KEY).users.getMetadataByToken(did || '')
 
   // Author a couple of cookies to persist a user's session
-  const token = await new SignJWT({user: JSON.stringify(user)})
+  const token = await new SignJWT({user: JSON.stringify(user), ...hasuraClaims})
   .setProtectedHeader({ alg: 'HS256' })
   .setJti(nanoid())
   .setIssuedAt()
-  .setExpirationTime('2h')
+  .setExpirationTime('1d')
   .sign(new TextEncoder().encode(process.env.ENCRYPTION_SECRET))
   CookieService.setTokenCookie(res, token)
 
