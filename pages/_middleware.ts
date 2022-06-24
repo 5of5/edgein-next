ca994c6984c9a30a8d962ada4211d40822511b48
Ed Parsons <ed@acutulus.co>
2022-06-23 22:23:23 -0400

    fixing before merge

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -5,7 +5,6 @@ import { mutate } from "../graphql/hasuraAdmin";
 
 export async function middleware(req: NextRequest) {
 	const url = req.nextUrl.clone();
-	console.log({url})
 
 	// Prevent security issues – users should not be able to canonically access
 	// the pages/sites folder and its respective contents. This can also be done
@@ -34,15 +33,6 @@ export async function middleware(req: NextRequest) {
 	) {
 		return NextResponse.next();
 	}
-	Object.defineProperty(global, '__DEV__', {
-		// In a buildless browser environment, maybe(() => process.env.NODE_ENV)
-		// evaluates as undefined, so __DEV__ becomes true by default, but can be
-		// initialized to false instead by a script/module that runs earlier.
-		value: process.env.NODE_ENV !== "production",
-		enumerable: false,
-		configurable: true,
-		writable: true,
-	});
 	let user;
 	try {
 		user = await CookieService.getUser(CookieService.getAuthToken(req.cookies));
@@ -52,7 +42,6 @@ export async function middleware(req: NextRequest) {
 			);
 		}
 	} catch (error) {
-		console.log(error);
 		return NextResponse.redirect(
 			new URL(`/login/?redirect=${encodeURIComponent(url.pathname)}`, req.url)
 		);
