8f3308dcdad55708b3c9c2b7a9d94a6bf4e05368
Ed Parsons <ed@acutulus.co>
2022-07-28 21:10:54 -0700

    fixing dev

--- scripts/airtable2postgres.ts
+++ scripts/airtable2postgres.ts
@@ -1,5 +1,5 @@
 import * as dotenv from 'dotenv';
-dotenv.config({ path: '../.env.aws' });
+dotenv.config({ path: './.env' });
 import { companiesMapping, investmentRoundsMapping, investmentsMapping, peopleMapping, vcFirmMapping, teamMembersMapping, Mapping, coinsMapping } from './mapping'
 import { getClient, upsertBatch } from './postgres_helpers'
 import { getAirtableTable } from './airtable_helpers'
