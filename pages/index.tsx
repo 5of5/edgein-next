6d53408071eb623556e9e6adc8af115dbee4154e
Sume <sume@technologies33.com>
2022-08-10 19:02:06 +0530

    added waitlist panel on landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import React, { useState } from "react";
 import Head from "next/head";
 // import Image from "next/image";
 import { FigureIntroSplash } from "../components/FigureIntroSplash";
@@ -6,9 +7,35 @@ import { FigureBlurredBg } from "../components/FigureBlurredBg";
 import { ElemButton } from "../components/ElemButton";
 // import styles from "../styles/Home.module.css";
 import { useAuth } from "../hooks/useAuth";
+import { InputText } from "../components/InputText";
+import { useFormspark } from "@formspark/use-formspark";
 
 const Home: NextPage = () => {
+	const [submit, submitting] = useFormspark({
+		formId: "Kz4dKDvu",
+	});
 	const { user, error, loading } = useAuth();
+	const [formSent, setFormSent] = useState(false);
+	const [email, setEmail] = useState("");
+
+	const onSubmit = async (e: { preventDefault: () => void }) => {
+		if (e) e.preventDefault();
+		await submit({
+			//name: name,
+			email: email,
+			_email: {
+				from: name,
+				subject: "Waitlist - EdgeIn",
+				template: {
+					title: false,
+					footer: false,
+				},
+			},
+		});
+
+		setFormSent(true);
+	};
+
 	return (
 		<>
 			<section className="relative">
@@ -26,7 +53,7 @@ const Home: NextPage = () => {
 								utility of blockchain technologies, to give you an unparalleled
 								edge in Web3.
 							</h2>
-							<div className="mt-8">
+							{/* <div className="mt-8">
 								{user ? (
 									<ElemButton href="/companies" btn="primary">
 										Explore
@@ -36,12 +63,52 @@ const Home: NextPage = () => {
 										Log In
 									</ElemButton>
 								)}
-							</div>
+							</div> */}
 						</div>
 						<div className="relative col-span-2 flex flex-col justify-center">
 							<FigureIntroSplash className="hidden lg:block relative" />
 						</div>
 					</div>
+					<div className="bg-white rounded-2xl border border-dark-500/10 p-6 w-[30rem]">
+					{/* <h1 className="text-3xl lg:text-4xl font-bold">
+						{formSent
+							? `You're on the list,!`
+							: "Join the waiting list"}
+					</h1> */}
+					<p className="mt-2 text-xl text-dark-400">
+						{formSent &&
+							 `your email ${email} has been added to our list.  we'll be in touch soon!`
+							//: "Explore Web3 data intelligence and contribute."
+						}
+					</p>
+
+					{!formSent && (
+						<>
+							<form
+								className="relative grid grid-cols-1 gap-y-4 mt-6 sm:grid-cols-5 sm:gap-x-4"
+								onSubmit={onSubmit}
+							>
+								<div className="group mb-6 sm:col-span-3">
+									<InputText
+										//label="Email"
+										type="email"
+										name="email"
+										value={email}
+										onChange={(e) => setEmail(e.target.value)}
+										placeholder="example@email.com"
+										required
+									/>
+								</div>
+								<div className="sm:col-span-2 mt-2">
+									<ElemButton roundedFull={true} btn="primary" loading={submitting}>
+										Join the waitlist
+									</ElemButton>
+								</div>
+							</form>
+						</>
+					)}
+				</div>
+						
 				</div>
 			</section>
 
