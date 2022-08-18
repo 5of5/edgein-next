9e3eeeab9e45e95ea4d4fae8b78bed85c7a64dcd
rainmakersdev11 <rainmakers.development11@rainmakers.org>
2022-08-18 13:27:31 +0530

    refactor external_id input

--- components/admin/companies.tsx
+++ components/admin/companies.tsx
@@ -3,6 +3,7 @@ import * as React from "react";
 import { List, Datagrid, Edit, Create, SimpleForm, TextField, EditButton, TextInput, SelectField, ReferenceField, NumberField, ReferenceInput, SelectInput, NumberInput } from 'react-admin';
 import BookIcon from '@mui/icons-material/Book';
 import uniqid from 'uniqid';
+import { toLower } from "lodash";
 export const companyIcon = BookIcon;
 
 export const CompanyList = () => (
@@ -129,60 +130,70 @@ export const CompanyEdit = () => (
   </Edit>
 );
 
-export const CompanyCreate = () => (
-  <Create title="Create a Company">
-    <SimpleForm>
-      <TextInput source="name" />
-      <TextInput source="slug" />
-      <SelectField source="layer" choices={[
-        {
-          id: "Layer 0",
-          name: "Layer 0 - Native Code"
-        },
-        {
-          id: "Layer 1",
-          name: "Layer 1 - Programmable Blockchains / Networks"
-        },
-        {
-          id: "Layer 2",
-          name: "Layer 2 - Nodes / Node Providers / Data Platforms"
-        },
-        {
-          id: "Layer 3",
-          name: "Layer 3 - API's / API Providers / Systems"
-        },
-        {
-          id: "Layer 4",
-          name: "Layer 4 - Decentralized Platforms / Contract / Modeling"
-        },
-        {
-          id: "Layer 5",
-          name: "Layer 5 - Applications"
-        },
-        {
-          id: "Layer 6",
-          name: "Layer 6 - Interoperable Digital Assets / NFT's"
-        },
-      ]} />
-      <TextInput source="layer_detail" />
-      <ReferenceInput label="Coin" source="coin_id" reference="coins">
-        <SelectInput optionText="name" />
-      </ReferenceInput>
-      <NumberInput source="total_employees" />
-      <TextInput source="github" />
-      <TextInput source="notes" />
-      <TextInput source="overview" />
-      <TextInput source="website" />
-      <TextInput source="careers_page" />
-      <TextInput source="company_linkedin" />
-      <NumberInput source="year_founded" min="1900" max="2099" />
-      <NumberInput source="investor_amount" />
-      <TextInput source="total_valuation" />
-      <TextInput source="white_paper" />
-      <TextInput source="market_verified" />
-      <TextInput source="velocity_linkedin" />
-      <TextInput source="velocity_token" />
-      <TextInput source="external_id" type="hidden" value={uniqid()} />
-    </SimpleForm>
-  </Create>
-);
+export const CompanyCreate = () => {
+  const [slug, setSlug] = React.useState('');
+  
+  const onChangeHandler = (event: any) => {
+    if (event.target.name === 'name') {
+      console.log(toLower(event.target.value).replace(' ', '-'))
+      setSlug(toLower(event.target.value).replace(' ', '-'))
+    }
+  }
+
+  return (
+    <Create title="Create a Company">
+      <SimpleForm onChange={onChangeHandler} defaultValues={{external_id: uniqid()}}>
+        <TextInput source="name" />
+        <TextInput source="slug" defaultValue={slug} onChange={(event) => setSlug(event.target.value)} />
+        <SelectField source="layer" choices={[
+          {
+            id: "Layer 0",
+            name: "Layer 0 - Native Code"
+          },
+          {
+            id: "Layer 1",
+            name: "Layer 1 - Programmable Blockchains / Networks"
+          },
+          {
+            id: "Layer 2",
+            name: "Layer 2 - Nodes / Node Providers / Data Platforms"
+          },
+          {
+            id: "Layer 3",
+            name: "Layer 3 - API's / API Providers / Systems"
+          },
+          {
+            id: "Layer 4",
+            name: "Layer 4 - Decentralized Platforms / Contract / Modeling"
+          },
+          {
+            id: "Layer 5",
+            name: "Layer 5 - Applications"
+          },
+          {
+            id: "Layer 6",
+            name: "Layer 6 - Interoperable Digital Assets / NFT's"
+          },
+        ]} />
+        <TextInput source="layer_detail" />
+        <ReferenceInput label="Coin" source="coin_id" reference="coins">
+          <SelectInput optionText="name" />
+        </ReferenceInput>
+        <NumberInput source="total_employees" />
+        <TextInput source="github" />
+        <TextInput source="notes" />
+        <TextInput source="overview" />
+        <TextInput source="website" />
+        <TextInput source="careers_page" />
+        <TextInput source="company_linkedin" />
+        <NumberInput source="year_founded" min="1900" max="2099" />
+        <NumberInput source="investor_amount" />
+        <TextInput source="total_valuation" />
+        <TextInput source="white_paper" />
+        <TextInput source="market_verified" />
+        <TextInput source="velocity_linkedin" />
+        <TextInput source="velocity_token" />
+      </SimpleForm>
+    </Create>
+  );
+}
