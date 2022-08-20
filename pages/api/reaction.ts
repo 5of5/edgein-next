99d347aa99db2b3163ebc30ad07a2c4d26b77a44
Ed Parsons <ed@acutulus.co>
2022-08-20 12:38:35 -0700

    Adding vc firms

--- pages/api/reaction.ts
+++ pages/api/reaction.ts
@@ -2,17 +2,118 @@ import { mutate, query } from '@/graphql/hasuraAdmin'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import CookieService from '../../utils/cookie'
 
+const increaseResourceSentiment = async (resourceType: 'companies' | 'vc_firms', resourceId: string, token: string, sentimentType: string, shouldInc: boolean, res: NextApiResponse) => {
+  if (resourceType === 'companies') {
+    return await increamentCompaniesSentiment(resourceId, token, sentimentType, shouldInc, res);
+  } else {
+    return increamentVCFirmsSentiment(resourceId, token, sentimentType, shouldInc, res);
+  }
+}
+
+const increamentCompaniesSentiment = async (companyId: string, token: string, sentimentType: string, shouldInc: boolean, res: NextApiResponse) => {
+  const { data } = await query({
+    query: `
+    query query_sentiment($companyId: Int!) {
+      companies_by_pk(id: $companyId) {
+        sentiment
+        slug
+      }
+    }`,
+    variables: {"companyId": companyId}
+  }, token)
+
+  const company = data.companies_by_pk || {}
+  const sentiment = company?.sentiment || {}
+
+  // Was added to list for first time, so increament company count
+  if (shouldInc) {
+    // update sentiment on company
+    // Feel this is bad and a race condition
+    // TODO refactor to use direct sql so increamenting can be atomic
+    if (!(sentimentType in sentiment)) {
+      sentiment[sentimentType] = 0
+    } 
+    sentiment[sentimentType] = sentiment[sentimentType] + 1
+
+    await mutate({
+      mutation: `
+      mutation update_sentiment($companyId: Int!, $sentiment: jsonb!) {
+        update_companies_by_pk(pk_columns: {id: $companyId}, _set: {sentiment: $sentiment}) {
+          sentiment
+        }
+      }    
+      `,
+      variables: {"companyId": companyId, sentiment}
+    }, token)
+
+    await res.unstable_revalidate(`/companies/${company.slug}/?revalidation_auth=${process.env.REVALIDATION_AUTH_TOKEN}`)
+  }
+
+  return sentiment
+}
+
+const increamentVCFirmsSentiment = async (vcFirmId: string, token: string, sentimentType: string, shouldInc: boolean, res: NextApiResponse) => {
+  const { data } = await query({
+    query: `
+    query query_sentiment($vcFirmId: Int!) {
+      vc_firms_by_pk(id: $vcFirmId) {
+        sentiment
+        slug
+      }
+    }`,
+    variables: {"vcFirmId": vcFirmId}
+  }, token)
+
+  const vcfirm = data.vc_firms_by_pk || {}
+  const sentiment = vcfirm?.sentiment || {}
+
+  // Was added to list for first time, so increament company count
+  if (shouldInc) {
+    // update sentiment on company
+    // Feel this is bad and a race condition
+    // TODO refactor to use direct sql so increamenting can be atomic
+    if (!(sentimentType in sentiment)) {
+      sentiment[sentimentType] = 0
+    } 
+    sentiment[sentimentType] = sentiment[sentimentType] + 1
+
+    await mutate({
+      mutation: `
+      mutation update_sentiment($vcFirmId: Int!, $sentiment: jsonb!) {
+        update_vc_firms_by_pk(pk_columns: {id: $vcFirmId}, _set: {sentiment: $sentiment}) {
+          sentiment
+        }
+      }    
+      `,
+      variables: {"vcFirmId": vcFirmId, sentiment}
+    }, token)
+
+    await res.unstable_revalidate(`/vcfirms/${vcfirm.slug}/?revalidation_auth=${process.env.REVALIDATION_AUTH_TOKEN}`)
+  }
+
+  return sentiment
+}
+
+
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== 'POST') return res.status(405).end()
 
   // params:
   const companyId: string = req.body.company
+  const vcFirmId: string = req.body.vcfirm
   const sentimentType: string = req.body.sentiment
   const pathname: string = req.body.pathname
 
   const token = CookieService.getAuthToken(req.cookies)
   const user = await CookieService.getUser(token);
   if (!user) return res.status(403).end()
+  
+  const resourceType = companyId ? 'companies' : vcFirmId ? 'vc_firms' : ''
+  const resourceId = resourceType === 'companies' ? companyId : vcFirmId
+
+  if (resourceType === '') {
+    return res.status(400).end()
+  }
 
   // console.log('starting reaction for user', {token,user,companyId,sentimentType,pathname})
   // check if user has a list for sentiment
@@ -56,49 +157,13 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     `,
     variables: {
       listId: list.id,
-      resourceId: companyId,
-      resourceType: "companies",
+      resourceId,
+      resourceType,
       userId: user.id
     }
   }, token)
 
-  const { data } = await query({
-    query: `
-    query query_sentiment($companyId: Int!) {
-      companies_by_pk(id: $companyId) {
-        sentiment
-        slug
-      }
-    }`,
-    variables: {"companyId": companyId}
-  }, token)
-
-  const company = data.companies_by_pk || {}
-  const sentiment = company?.sentiment || {}
-
-  // Was added to list for first time, so increament company count
-  if (insert_follows_one) {
-    // update sentiment on company
-    // Feel this is bad and a race condition
-    // TODO refactor to use direct sql so increamenting can be atomic
-    if (!(sentimentType in sentiment)) {
-      sentiment[sentimentType] = 0
-    } 
-    sentiment[sentimentType] = sentiment[sentimentType] + 1
-
-    await mutate({
-      mutation: `
-      mutation update_sentiment($companyId: Int!, $sentiment: jsonb!) {
-        update_companies_by_pk(pk_columns: {id: $companyId}, _set: {sentiment: $sentiment}) {
-          sentiment
-        }
-      }    
-      `,
-      variables: {"companyId": companyId, sentiment}
-    }, token)
-
-    await res.unstable_revalidate(`/companies/${company.slug}/?revalidation_auth=${process.env.REVALIDATION_AUTH_TOKEN}`)
-  }
+  const sentiment = increaseResourceSentiment(resourceType, resourceId, token, sentimentType, Boolean(insert_follows_one), res)
 
   // create action
   mutate({
@@ -117,8 +182,8 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         page: pathname,
         properties: {
           listId: list.id,
-          resourceId: companyId,
-          resourceType: "companies",
+          resourceId,
+          resourceType,
           sentiment: sentimentType,
         },
         user: user.email,
