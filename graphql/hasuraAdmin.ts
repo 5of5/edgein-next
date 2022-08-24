f765e92ee3bf7a63b6866a25a3a1b83c541d633f
Nargis Parween <nargis.parween@unthinkable.co>
2022-08-24 08:17:00 +0530

    Added hasura Mutate

--- graphql/hasuraAdmin.ts
+++ graphql/hasuraAdmin.ts
@@ -1,6 +1,30 @@
+async function hasuraMutate<TResult = any>(
+  args: {mutation: string, variables: Record<string, any>},
+) {
+  const opts = {
+    method: "POST",
+    body: JSON.stringify({
+      query: args.mutation,
+      variables: args.variables
+    }),
+    headers:   {
+      'x-hasura-role': 'admin',
+      'x-hasura-admin-secret': process.env.HASURA_ADMIN_SECRET ?? ""
+    }        
+  }
+  const res = await fetch(process.env.GRAPHQL_ENDPOINT ?? "", opts);
+
+  const json = await res.json()
+
+  if (json.errors) {
+    throw json.errors
+  }
+
+  return json as TResult
+}
+
 async function hasuraQuery<TResult = any>(
   args: {query: string, variables: Record<string, any>},
-  token?: string,
 ) {
   const opts = {
     method: "POST",
@@ -13,25 +37,21 @@ async function hasuraQuery<TResult = any>(
       'x-hasura-admin-secret': process.env.HASURA_ADMIN_SECRET ?? ""
     }        
   }
-  if (token) {
-    // switch to user creds
-  }
   const res = await fetch(process.env.GRAPHQL_ENDPOINT ?? "", opts);
 
   const json = await res.json()
 
   if (json.errors) {
-    console.log('gql errors', json.errors)
     throw json.errors
   }
 
   return json as TResult
 }
 
-export async function mutate<TResult = any>(args: {mutation: string, variables: Record<string, any>}, token?: string) {
-  return hasuraQuery<TResult>({query: args.mutation, variables: args.variables}, token);
+export async function mutate<TResult = any>(args: {mutation: string, variables: Record<string, any>}) {
+  return hasuraMutate<TResult>(args);
 }
 
-export async function query<TResult = any>(args: {query: string, variables: Record<string, any>}, token?: string) {
-  return hasuraQuery<TResult>(args, token);
+export async function query<TResult = any>(args: {query: string, variables: Record<string, any>}) {
+  return hasuraQuery<TResult>(args);
 }
