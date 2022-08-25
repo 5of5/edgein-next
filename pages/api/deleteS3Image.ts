ccbca85c34f492a077c03921ab4d71ba2a65e55e
Nargis <rainmakers.development13@rainmakers.org>
2022-08-25 18:04:48 +0530

    Fixed the typescript error

--- pages/api/deleteS3Image.ts
+++ pages/api/deleteS3Image.ts
@@ -14,7 +14,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
   try {
     // get extension of the file, prefix with  a dot if extension is found
-    let fileName = req.query.file;
+    const fileName:string = req.query.file as string
 
     // create s3 instance.
     const s3 = new AWS.S3({
@@ -22,7 +22,9 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       region: process.env.AWS_REGION!,
     });
 
-    await s3.deleteObject({ Bucket: process.env.AWS_BUCKET, Key: fileName }).promise();
+    const objects = { Bucket: process.env.AWS_BUCKET!, Key: fileName };
+
+    await s3.deleteObject(objects).promise();
     res.send({ success: true })
   } catch (err: any) {
     console.log(err);
