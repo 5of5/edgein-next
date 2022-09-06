095a82cba1334bdf4476f286f7f788d3e7b3c378
raylopro <contact@raylopro.com>
2022-09-06 13:57:59 -0700

    modal update (removed css & used tailwind)

--- components/ForgotPasswordModal.tsx
+++ components/ForgotPasswordModal.tsx
@@ -1,117 +1,117 @@
 import type { GetStaticProps } from "next";
 import { useState } from "react";
 import { ElemButton } from "./ElemButton";
-import Modal from 'react-modal';
+import Modal from "react-modal";
 
-Modal.setAppElement('#modal-root');
+Modal.setAppElement("#modal-root");
 type Props = {
-    show: boolean;
-    onClose:() => void;
-    onBack:() => void;
+	show: boolean;
+	onClose: () => void;
+	onBack: () => void;
 };
 
 export default function ForgotPasswordModal(props: Props) {
+	const [email, setEmail] = useState("");
+	const [isLoading, setIsLoading] = useState(false);
+	const [isMailSent, setIsMailSent] = useState(false);
 
-    const [email, setEmail] = useState("");
-    const [isLoading, setIsLoading] = useState(false);
-    const [isMailSent, setIsMailSent] = useState(false)
+	const handleSubmit = async () => {
+		if (!email) {
+			alert("Enter email!");
+			return;
+		}
+		try {
+			const response = await fetch("/api/change_password/", {
+				method: "POST",
+				headers: {
+					Accept: "application/json",
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ email }),
+			}).then((res) => res.json());
+			if (response.success === true) {
+				setIsMailSent(true);
+			}
+		} catch (e) {
+			// setIsMailSent(true)
 
-    const handleSubmit = async () => {
-        if(!email){
-            alert("Enter email!")
-            return;
-        }
-        try {
-            const response = await fetch("/api/change_password/", {
-                method: "POST",
-                headers: {
-                    Accept: "application/json",
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ email}),
-            }).then(res => res.json());
-            if (response.success === true) {
-                setIsMailSent(true)
-            }
+			setIsLoading(false);
+		}
+	};
 
-        } catch (e) {
-            // setIsMailSent(true)
-           
-            setIsLoading(false);
-        }
-    };
+	const onClose = () => {
+		props.onClose();
+	};
 
-    const onClose = () => {
-        props.onClose();
-    }
+	const onBack = () => {
+		props.onBack();
+	};
 
-    const onBack = () => {
-        props.onBack();
-    }
-
-    return (
-        <Modal
-            isOpen={props.show}
-            // onAfterOpen={afterOpenModal}
-            onRequestClose={onClose}
-            overlayClassName="fixed top-0 left-0 z-[50] flex items-center justify-center h-screen w-screen p-6 cursor-auto bg-black/20 backdrop-blur-sm"
-            className={`${
-                "animate-fade-in-up"
-            } relative z-[50] max-w-sm w-full mx-auto my-0 min-h-0 flex flex-col rounded-lg shadow-2xl bg-white overflow-y-scroll overflow-x-hidden focus:outline-none focus:ring-0`}
-            
-            contentLabel="Forgot Password Modal"
-        >
-           
-            <div className="relative max-w-md mx-auto ">
-                <div className="bg-white rounded-2xl p-6">
-                    {
-                        isMailSent ? 
-                        <>
-                        <h1 className="text-center text-2xl lg:text-3xl font-bold">Email Sent</h1>
-                                <p className="mt-2 text-md text-dark-400 text-center">
-                                    {`We sent an email to ${email}! if this email is connected to an EdgeIn account, you'll be able to reset your password.`}
-                                </p>
-                                <div className="text-center sm:col-span-3 mt-10">
-                                    <ElemButton className="w-full" onClick={onBack} btn="primary" loading={isLoading}>
-                                        Back to login
-                                </ElemButton>
-                                </div>
-                        </>
-                        :
-                        <>
-                               
-                        <h1 className="text-center text-2xl lg:text-3xl font-bold mt-6">Reset Password</h1>
-                        {/* <p className="mt-2 text-xl text-dark-400">
+	return (
+		<Modal
+			isOpen={props.show}
+			onRequestClose={onClose}
+			overlayClassName="fixed top-0 left-0 z-[50] flex items-center justify-center h-screen w-screen p-6 cursor-auto bg-black/20 backdrop-blur-sm"
+			className="animate-fade-in-up relative z-[50] max-w-sm w-full mx-auto my-0 min-h-0 flex flex-col rounded-lg shadow-2xl bg-white overflow-y-scroll overflow-x-hidden focus:outline-none focus:ring-0"
+			contentLabel="Forgot Password Modal"
+		>
+			<div className="relative max-w-md mx-auto ">
+				<div className="bg-white rounded-2xl p-6">
+					{isMailSent ? (
+						<>
+							<h1 className="text-center text-2xl lg:text-3xl font-bold">
+								Email Sent
+							</h1>
+							<p className="mt-2 text-md text-dark-400 text-center">
+								{`We sent an email to ${email}! if this email is connected to an EdgeIn account, you'll be able to reset your password.`}
+							</p>
+							<div className="text-center sm:col-span-3 mt-10">
+								<ElemButton
+									className="w-full"
+									onClick={onBack}
+									btn="primary"
+									loading={isLoading}
+								>
+									Back to login
+								</ElemButton>
+							</div>
+						</>
+					) : (
+						<>
+							<h1 className="text-center text-2xl lg:text-3xl font-bold mt-6">
+								Reset Password
+							</h1>
+							{/* <p className="mt-2 text-xl text-dark-400">
                             {"A recovery link will be sent to the email below."}
                         </p> */}
-                        <div
-                            className="relative grid grid-cols-1 gap-y-4 mt-6 sm:grid-cols-2 sm:gap-x-8"
-                        >
-                            <div className="group sm:col-span-2">
-                               
-                                <input
-                                    name="email"
-                                    type="email"
-                                    value={email}
-                                    disabled={isLoading}
-                                    onChange={(event) => setEmail(event ?.target.value)}
-                                    placeholder="example@email.com"
-                                    className="w-full mt-1 px-3 py-1.5 text-lg text-dark-500 relative bg-white rounded-md border border-slate-300 outline-none placeholder:text-gray-300  focus:outline-none focus:border-primary-500 focus:ring focus:ring-primary-100"
-                                />
-                            </div>
+							<div className="relative grid grid-cols-1 gap-y-4 mt-6 sm:grid-cols-2 sm:gap-x-8">
+								<div className="group sm:col-span-2">
+									<input
+										name="email"
+										type="email"
+										value={email}
+										disabled={isLoading}
+										onChange={(event) => setEmail(event?.target.value)}
+										placeholder="example@email.com"
+										className="w-full mt-1 px-3 py-1.5 text-lg text-dark-500 relative bg-white rounded-md border border-slate-300 outline-none placeholder:text-gray-300  focus:outline-none focus:border-primary-500 focus:ring focus:ring-primary-100"
+									/>
+								</div>
 
-                            <div className="text-right sm:col-span-2">
-                                <ElemButton className="w-full" onClick={handleSubmit} btn="primary" loading={isLoading}>
-                                    Send a password reset email
-                            </ElemButton>
-                            </div>
-                        </div>
-                    </>
-               
-                    }
-                          
-                </div>
-            </div>
-        </Modal>
-    )
+								<div className="text-right sm:col-span-2">
+									<ElemButton
+										className="w-full"
+										onClick={handleSubmit}
+										btn="primary"
+										loading={isLoading}
+									>
+										Send a password reset email
+									</ElemButton>
+								</div>
+							</div>
+						</>
+					)}
+				</div>
+			</div>
+		</Modal>
+	);
 }
--- components/SignUpModal.tsx
+++ components/SignUpModal.tsx
@@ -2,233 +2,255 @@ import type { GetStaticProps } from "next";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { ElemButton } from "../components/ElemButton";
-import Modal from 'react-modal';
+import Modal from "react-modal";
 import { ElemLogo } from "./ElemLogo";
-const validator = require('validator');
+const validator = require("validator");
 
-Modal.setAppElement('#modal-root');
+Modal.setAppElement("#modal-root");
 
 type Props = {
-    show: boolean;
-    passwordFromLogin: string;
-    emailFromLogin: string;
-    onLogin: () => void;
-    onClose:() => void
+	show: boolean;
+	passwordFromLogin: string;
+	emailFromLogin: string;
+	onLogin: () => void;
+	onClose: () => void;
 };
 
 export default function SignUpModal(props: Props) {
-    const router = useRouter();
+	const router = useRouter();
 
-    const [isBrowser, setIsBrowser] = useState(false);
-    const [errorMessage, setErrorMessage] = useState('')
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState(props.emailFromLogin ? props.emailFromLogin : "");
-    const [password, setPassword] = useState(props.passwordFromLogin ? props.passwordFromLogin : "")
-    const [isSignUp, setIsSignUp] = useState(false);
-    const [isLoading, setIsLoading] = useState(false);
-    const [isWaitlisted, setIsWaitlisted] = useState(false)
-    const [isRegistered, setIsRegistered] = useState(false)
-    const [emailError, setEmailError] = useState('')
-    const [nameError, setNameError] = useState('')
-    // const [finishingLogin, setFinishingLogin] = useState(
-    //     Boolean(router.query.email)
-    // );
+	const [isBrowser, setIsBrowser] = useState(false);
+	const [errorMessage, setErrorMessage] = useState("");
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState(
+		props.emailFromLogin ? props.emailFromLogin : ""
+	);
+	const [password, setPassword] = useState(
+		props.passwordFromLogin ? props.passwordFromLogin : ""
+	);
+	const [isSignUp, setIsSignUp] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
+	const [isWaitlisted, setIsWaitlisted] = useState(false);
+	const [isRegistered, setIsRegistered] = useState(false);
+	const [emailError, setEmailError] = useState("");
+	const [nameError, setNameError] = useState("");
+	// const [finishingLogin, setFinishingLogin] = useState(
+	//     Boolean(router.query.email)
+	// );
 
-    const validate = (value: string) => {
-        setPassword(value)
-        if (validator.isStrongPassword(value, {
-            minLength: 8, minLowercase: 1,
-            minUppercase: 1, minNumbers: 1, minSymbols: 1
-        })) {
-            setErrorMessage('')
-        } else {
-            setErrorMessage('Password should have least 8 characters including a lower-case letter, an upper-case letter, a number, a special character')
-        }
-    }
+	const validate = (value: string) => {
+		setPassword(value);
+		if (
+			validator.isStrongPassword(value, {
+				minLength: 8,
+				minLowercase: 1,
+				minUppercase: 1,
+				minNumbers: 1,
+				minSymbols: 1,
+			})
+		) {
+			setErrorMessage("");
+		} else {
+			setErrorMessage(
+				"Password should have least 8 characters including a lower-case letter, an upper-case letter, a number, a special character"
+			);
+		}
+	};
 
-    const validateEmail = (value: string) => {
-        setEmail(value)
-        if (validator.isEmail(value)) {
-            setEmailError('')
-        } else {
-            setEmailError('Enter valid Email!')
-        }
-    }
+	const validateEmail = (value: string) => {
+		setEmail(value);
+		if (validator.isEmail(value)) {
+			setEmailError("");
+		} else {
+			setEmailError("Enter valid Email!");
+		}
+	};
 
-    const validateName = (value: string) => {
-        setName(value)
-        if (value.length > 0) {
-            setNameError('')
-        } else {
-            setNameError('Enter Name!')
-        }
-    }
+	const validateName = (value: string) => {
+		setName(value);
+		if (value.length > 0) {
+			setNameError("");
+		} else {
+			setNameError("Enter Name!");
+		}
+	};
 
-    useEffect(() => {
-        setIsBrowser(true);
-        setName('')
-        setEmail(props.emailFromLogin ? props.emailFromLogin : "");
-        setPassword(props.passwordFromLogin ? props.passwordFromLogin : "")
-        setIsSignUp(false)
-        setIsWaitlisted(false)
-        setIsRegistered(false)
-        setErrorMessage('')
-        setEmailError('')
-        setNameError('')
-    }, [props.show,props.emailFromLogin, props.passwordFromLogin]);
+	useEffect(() => {
+		setIsBrowser(true);
+		setName("");
+		setEmail(props.emailFromLogin ? props.emailFromLogin : "");
+		setPassword(props.passwordFromLogin ? props.passwordFromLogin : "");
+		setIsSignUp(false);
+		setIsWaitlisted(false);
+		setIsRegistered(false);
+		setErrorMessage("");
+		setEmailError("");
+		setNameError("");
+	}, [props.show, props.emailFromLogin, props.passwordFromLogin]);
 
+	const onLogin = () => {
+		// event.preventDefault();
+		// setIsLoading(true);
+		props.onLogin();
+	};
 
+	const onSignUp = async () => {
+		validateEmail(email);
+		validateName(name);
+		validate(password);
+		if (!name || !email || !password) {
+			return;
+		}
 
-    const onLogin = () => {
-        // event.preventDefault();
-        // setIsLoading(true);
-        props.onLogin()
-    };
+		try {
+			const response = await fetch("/api/register/", {
+				method: "POST",
+				headers: {
+					Accept: "application/json",
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ email, password, name }),
+			}).then((res) => res.json());
+			if (response.success === true) {
+				setIsRegistered(true);
+			}
+		} catch (e) {
+			setIsWaitlisted(true);
+			//console.log(e);
+			setIsLoading(false);
+		}
+	};
 
-    const onSignUp = async () => {
-        validateEmail(email)
-        validateName(name)
-        validate(password)
-        if (!name || !email || !password) {
-            return;
-        }
-        
-        try {
-            const response = await fetch("/api/register/", {
-                method: "POST",
-                headers: {
-                    Accept: "application/json",
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ email, password, name }),
-            }).then(res => res.json());
-            if (response.success === true) {
-                setIsRegistered(true)
-            }
+	const onClose = () => {
+		props.onClose();
+	};
 
-        } catch (e) {
-            setIsWaitlisted(true)
-            //console.log(e);
-            setIsLoading(false);
-        }
-    };
-
-    const onClose = () => {
-        props.onClose();
-    }
-
-    return (
-        <Modal
-            isOpen={props.show}
-            // onAfterOpen={afterOpenModal}
-            onRequestClose={onClose}
-            overlayClassName="fixed top-0 left-0 z-[50] flex items-center justify-center h-screen w-screen p-6 cursor-auto bg-black/20 backdrop-blur-sm"
-            className={`${
-                "animate-fade-in-up"
-            } relative z-[50] max-w-sm w-full mx-auto my-0 min-h-0 flex flex-col rounded-lg shadow-2xl bg-white overflow-y-scroll overflow-x-hidden focus:outline-none focus:ring-0`}
-            
-            contentLabel="SignUp Modal"
-        >
-            <div className="relative max-w-md mx-auto">
-                <div className="bg-white rounded-2xl p-10 center">
-                    {
-
-                        isRegistered ? (
-                            <>
-                                <h1 className="text-center text-2xl lg:text-3xl font-bold">Registration Complete</h1>
-                                <p className="mt-2 text-md text-dark-400 text-center">
-                                    {`User has been successfully registered. Please Login!`}
-                                </p>
-                                <div className="text-center sm:col-span-3 mt-10">
-                                    <ElemButton className="w-full" onClick={onLogin} btn="primary" loading={isLoading}>
-                                        Back to login
-                                </ElemButton>
-                                </div>
-                            </>
-
-                        )
-                            :
-
-                            isWaitlisted ? (
-                                <>
-                                    {/* <h1 className="text-center text-2xl lg:text-3xl font-bold">Registration Complete</h1> */}
-                                    <p className="mt-2 text-md text-dark-400 text-center">
-                                        {`Your email ${email} has been added to our waitlist.  We'll be in touch soon!`}
-                                    </p>
-                                </>
-    
-                            ):
-
-                            <>
-                                <ElemLogo
-                                    mode="icon"
-                                    className="text-center h-8  scale-90 self-center  mx-32 mb-10"
-                                />
-                                <h1 className="text-center text-2xl lg:text-3xl font-bold">Welcome to EdgeIn</h1>
-                                {/* <div className="text-center sm:col-span-3 mt-5">
+	return (
+		<Modal
+			isOpen={props.show}
+			// onAfterOpen={afterOpenModal}
+			onRequestClose={onClose}
+			overlayClassName="fixed top-0 left-0 z-[50] flex items-center justify-center h-screen w-screen p-6 cursor-auto bg-black/20 backdrop-blur-sm"
+			className="animate-fade-in-up relative z-[50] max-w-sm w-full mx-auto my-0 min-h-0 flex flex-col rounded-lg shadow-2xl bg-white overflow-y-scroll overflow-x-hidden focus:outline-none focus:ring-0"
+			contentLabel="SignUp Modal"
+		>
+			<div className="relative max-w-md mx-auto">
+				<div className="bg-white rounded-2xl p-10 center">
+					{isRegistered ? (
+						<>
+							<h1 className="text-center text-2xl lg:text-3xl font-bold">
+								Registration Complete
+							</h1>
+							<p className="mt-2 text-md text-dark-400 text-center">
+								{`User has been successfully registered. Please Login!`}
+							</p>
+							<div className="text-center sm:col-span-3 mt-10">
+								<ElemButton
+									className="w-full"
+									onClick={onLogin}
+									btn="primary"
+									loading={isLoading}
+								>
+									Back to login
+								</ElemButton>
+							</div>
+						</>
+					) : isWaitlisted ? (
+						<>
+							{/* <h1 className="text-center text-2xl lg:text-3xl font-bold">Registration Complete</h1> */}
+							<p className="mt-2 text-md text-dark-400 text-center">
+								{`Your email ${email} has been added to our waitlist.  We'll be in touch soon!`}
+							</p>
+						</>
+					) : (
+						<>
+							<ElemLogo
+								mode="icon"
+								className="text-center h-8  scale-90 self-center  mx-32 mb-10"
+							/>
+							<h1 className="text-center text-2xl lg:text-3xl font-bold">
+								Welcome to EdgeIn
+							</h1>
+							{/* <div className="text-center sm:col-span-3 mt-5">
                                     <ElemButton roundedFull={false} className="w-full rounded-md text-blue-md border border-slate-300" onClick={() => { }} btn="ol-primary" >
                                         Continue with LinkedIn
                                 </ElemButton>
                                 </div> */}
-                                {/* <div className="text-center sm:col-span-3 mt-5">
+							{/* <div className="text-center sm:col-span-3 mt-5">
                                     <span className="text-gray-300 text-sm text-light">----------------------------<b className="text-dark-600">{` or `}</b>----------------------------</span>
                                 </div> */}
-                                <div
-                                    className="text-center relative grid grid-cols-1 gap-y-4 mt-6 sm:grid-cols-1 sm:gap-x-0"
-                                >
-                                    <div className="group sm:col-span-1">
-                                        <input
-                                            name="name"
-                                            type="text"
-                                            value={name}
-                                            disabled={isLoading}
-                                            onChange={(event) => validateName(event ?.target.value)}
-                                            placeholder="Name"
-                                            className="w-full mt-1 px-3 py-1.5 text-md text-dark-500 relative bg-white rounded-md border border-slate-300 outline-none placeholder:text-gray-300  focus:outline-none focus:border-primary-500 focus:ring focus:ring-primary-100"
-                                        />
-                                        {nameError === '' ? null :
-                                            <span className="w-full text-start text-sm">{nameError}</span>}
-                                        <input
-                                            name="email"
-                                            type="email"
-                                            value={email}
-                                            disabled={isLoading || props.emailFromLogin != ''}
-                                            onChange={(event) => validateEmail(event ?.target.value)}
-                                            placeholder="Email"
-                                            className="w-full mt-1 px-3 py-1.5 text-md text-dark-500 relative bg-white rounded-md border border-slate-300 outline-none placeholder:text-gray-300  focus:outline-none focus:border-primary-500 focus:ring focus:ring-primary-100"
-                                        />
-                                        {emailError === '' ? null :
-                                            <span className="w-full text-start text-sm">{emailError}</span>}
-                                        <input
-                                            name="password"
-                                            type="password"
-                                            value={password}
-                                            disabled={isLoading || props.passwordFromLogin != ''}
-                                            onChange={(event) => validate(event ?.target.value)}
-                                            placeholder="Password"
-                                            className="w-full mt-1 px-3 py-1.5 text-md text-dark-500 relative bg-white rounded-md border border-slate-300 outline-none placeholder:text-gray-300  focus:outline-none focus:border-primary-500 focus:ring focus:ring-primary-100"
-                                        />
-                                        {errorMessage === '' ? null :
-                                            <span className="w-full text-start text-sm">{errorMessage}</span>}
+							<div className="text-center relative grid grid-cols-1 gap-y-4 mt-6 sm:grid-cols-1 sm:gap-x-0">
+								<div className="group sm:col-span-1">
+									<input
+										name="name"
+										type="text"
+										value={name}
+										disabled={isLoading}
+										onChange={(event) => validateName(event?.target.value)}
+										placeholder="Name"
+										className="w-full mt-1 px-3 py-1.5 text-md text-dark-500 relative bg-white rounded-md border border-slate-300 outline-none placeholder:text-gray-300  focus:outline-none focus:border-primary-500 focus:ring focus:ring-primary-100"
+									/>
+									{nameError === "" ? null : (
+										<span className="w-full text-start text-sm">
+											{nameError}
+										</span>
+									)}
+									<input
+										name="email"
+										type="email"
+										value={email}
+										disabled={isLoading || props.emailFromLogin != ""}
+										onChange={(event) => validateEmail(event?.target.value)}
+										placeholder="Email"
+										className="w-full mt-1 px-3 py-1.5 text-md text-dark-500 relative bg-white rounded-md border border-slate-300 outline-none placeholder:text-gray-300  focus:outline-none focus:border-primary-500 focus:ring focus:ring-primary-100"
+									/>
+									{emailError === "" ? null : (
+										<span className="w-full text-start text-sm">
+											{emailError}
+										</span>
+									)}
+									<input
+										name="password"
+										type="password"
+										value={password}
+										disabled={isLoading || props.passwordFromLogin != ""}
+										onChange={(event) => validate(event?.target.value)}
+										placeholder="Password"
+										className="w-full mt-1 px-3 py-1.5 text-md text-dark-500 relative bg-white rounded-md border border-slate-300 outline-none placeholder:text-gray-300  focus:outline-none focus:border-primary-500 focus:ring focus:ring-primary-100"
+									/>
+									{errorMessage === "" ? null : (
+										<span className="w-full text-start text-sm">
+											{errorMessage}
+										</span>
+									)}
+								</div>
 
-                                    </div>
-
-                                    <div className="text-center sm:col-span-3">
-                                        <ElemButton className="w-full" onClick={onSignUp} btn="primary" loading={isLoading}>
-                                            Sign up and explore
-                                </ElemButton>
-                                    </div>
-                                    <p className="text-sm text-gray-300">{`Creating an account means you're okay with our terms of service, privact policy, and our default notification settings.`}</p>
-                                    <div className="text-center sm:col-span-3">
-                                        <ElemButton className="w-full" onClick={onLogin} btn="ol-primary" loading={isLoading}>
-                                            Login
-                                </ElemButton>
-                                    </div>
-                                </div>
-                            </>
-                    }
-                </div>
-            </div>
-        </Modal>
-    )
+								<div className="text-center sm:col-span-3">
+									<ElemButton
+										className="w-full"
+										onClick={onSignUp}
+										btn="primary"
+										loading={isLoading}
+									>
+										Sign up and explore
+									</ElemButton>
+								</div>
+								<p className="text-sm text-gray-300">{`Creating an account means you're okay with our terms of service, privact policy, and our default notification settings.`}</p>
+								<div className="text-center sm:col-span-3">
+									<ElemButton
+										className="w-full"
+										onClick={onLogin}
+										btn="ol-primary"
+										loading={isLoading}
+									>
+										Login
+									</ElemButton>
+								</div>
+							</div>
+						</>
+					)}
+				</div>
+			</div>
+		</Modal>
+	);
 }
