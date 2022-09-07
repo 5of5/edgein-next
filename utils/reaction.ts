f96f2f55695120bd98561e079271ebd3c5739557
raylopro <contact@raylopro.com>
2022-09-06 19:16:09 -0700

    getName error temp fix

--- utils/reaction.ts
+++ utils/reaction.ts
@@ -1,62 +1,78 @@
-import { Follows_Companies, Follows_Vc_Firms, Lists } from "@/graphql/types"
-import { find, has } from "lodash"
+import { Follows_Companies, Follows_Vc_Firms, Lists } from "@/graphql/types";
+import { find, has } from "lodash";
 
 type ReactionType = {
-  company?: number
-  vcfirm?: number
-  sentiment: string
-  pathname: string
-}
-
-export const reactOnSentiment = async ({
-  company,
-  vcfirm,
-  sentiment,
-  pathname
-}: ReactionType) => {
-  const resp = await fetch("/api/reaction/", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      company,
-      vcfirm,
-      sentiment,
-      pathname
-    }),
-  });
-  return resp.json()
-}
-
-export const getNewFollows = (sentiment: string, type: string = 'company') => {
-  const follows = type === 'company' ? {} as Follows_Companies : {} as Follows_Vc_Firms;
-  follows.list = {} as Lists;
-  follows.list.name = `sentiment-${sentiment}`;
-
-  return follows;
-}
-
-export const getName = (list: Lists) => {
-  const fragments = list.name.split('-');
-  return fragments[fragments.length - 1];
+	company?: number;
+	vcfirm?: number;
+	sentiment: string;
+	pathname: string;
 };
 
-export const isFollowsExists = (follows: Follows_Companies[] | Follows_Vc_Firms[], sentiment: string) => {
-  return find(follows, (follow: Follows_Companies | Follows_Vc_Firms) => getName(follow?.list!) === sentiment)
-}
+export const reactOnSentiment = async ({
+	company,
+	vcfirm,
+	sentiment,
+	pathname,
+}: ReactionType) => {
+	const resp = await fetch("/api/reaction/", {
+		method: "POST",
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			company,
+			vcfirm,
+			sentiment,
+			pathname,
+		}),
+	});
+	return resp.json();
+};
 
-export const getNewTempSentiment = (sentiments: any, sentiment: string, alreadyReacted: boolean) => {
+export const getNewFollows = (sentiment: string, type: string = "company") => {
+	const follows =
+		type === "company" ? ({} as Follows_Companies) : ({} as Follows_Vc_Firms);
+	follows.list = {} as Lists;
+	follows.list.name = `sentiment-${sentiment}`;
 
-  const newSentiment = { ...sentiments }
+	return follows;
+};
 
-  const hasSentiment = has(newSentiment, sentiment)
+export const getName = (list: Lists) => {
+	if (list) {
+		const fragments = list.name.split("-");
+		return fragments[fragments.length - 1];
+	}
+};
 
-  if (!hasSentiment && alreadyReacted) { }
-  else if (!hasSentiment && !alreadyReacted) newSentiment[sentiment] = 1
-  else if (hasSentiment && !alreadyReacted) newSentiment[sentiment] += 1
-  else if (hasSentiment && alreadyReacted) newSentiment[sentiment] > 0 ? newSentiment[sentiment] -= 1 : newSentiment[sentiment] = 0
+export const isFollowsExists = (
+	follows: Follows_Companies[] | Follows_Vc_Firms[],
+	sentiment: string
+) => {
+	return find(
+		follows,
+		(follow: Follows_Companies | Follows_Vc_Firms) =>
+			getName(follow?.list!) === sentiment
+	);
+};
 
-  return newSentiment
-}
+export const getNewTempSentiment = (
+	sentiments: any,
+	sentiment: string,
+	alreadyReacted: boolean
+) => {
+	const newSentiment = { ...sentiments };
+
+	const hasSentiment = has(newSentiment, sentiment);
+
+	if (!hasSentiment && alreadyReacted) {
+	} else if (!hasSentiment && !alreadyReacted) newSentiment[sentiment] = 1;
+	else if (hasSentiment && !alreadyReacted) newSentiment[sentiment] += 1;
+	else if (hasSentiment && alreadyReacted)
+		newSentiment[sentiment] > 0
+			? (newSentiment[sentiment] -= 1)
+			: (newSentiment[sentiment] = 0);
+
+	return newSentiment;
+};
