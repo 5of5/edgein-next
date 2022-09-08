d1647a559ef3e46450eb6d90bdb0e1c94dc62493
Sagar Guhe <rainmakers.development11@rainmakers.org>
2022-09-08 12:30:43 +0530

    add multiple_resources_to_list api

---
+++ pages/api/multiple_resources_to_list.ts
@@ -0,0 +1,45 @@
+import { NextApiResponse, NextApiRequest } from "next";
+import CookieService from '../../utils/cookie'
+import { has, map } from "lodash";
+import { updateResourceSentimentCount, upsertFollow, upsertList } from "@/utils/lists";
+
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+
+  if (req.method !== 'POST') return res.status(405).end()
+
+  const token = CookieService.getAuthToken(req.cookies)
+  const user = await CookieService.getUser(token);
+  if (!user) return res.status(403).end()
+
+  const sentimentType: string = req.body.sentiment
+
+  const resourceType = has(req.body, 'companies') ? 'companies' : has(req.body, 'vcfirms') ? 'vc_firms' : ''
+
+  if (resourceType === '') {
+    return res.status(400).end()
+  }
+
+  // console.log('starting reaction for user', {token,user,companyId,sentimentType,pathname})
+  // check if user has a list for sentiment
+  const listname = `sentiment-${user.id}-${sentimentType}`
+  // upsertList
+  const list = await upsertList(listname, user, token)
+
+  const reactions = await Promise.all(map(req.body.companies, async (resource) => {
+    // insert follow only if the follows don't exists
+    const follow = await upsertFollow(list, resource.company, resourceType, user, token)
+
+    const { sentiment, revalidatePath } = await updateResourceSentimentCount(resourceType, resource.company, token, sentimentType, Boolean(follow), false)
+
+    if (revalidatePath) {
+      await res.unstable_revalidate(revalidatePath)
+    }
+
+    return { company: resource.company, sentiment: { ...sentiment } }
+  }))
+
+  res.send({ reactions })
+}
+
+export default handler
