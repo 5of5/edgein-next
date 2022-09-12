385e5aae6eade4b247e8ba16278364c13f414baf
Nargis <rainmakers.development13@rainmakers.org>
2022-09-12 15:49:59 +0530

    added the fields for auth0 linkedId and userPassId

--- models/User.ts
+++ models/User.ts
@@ -5,4 +5,6 @@ export type User = {
   display_name?: string | null
   role: "user" | "admin"
   is_auth0_verified: boolean
+  auth0_linkedin_id?: string | null
+  auth0_user_pass_id?: string | null
 }
