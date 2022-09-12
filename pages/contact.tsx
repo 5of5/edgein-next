35a08fcf83cfe116fb9b20ae35d9957d8e883c04
raylopro <contact@raylopro.com>
2022-09-12 13:06:25 -0700

    contact/team/images

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,15 +1,11 @@
 import React, { useState } from "react";
 import type { NextPage, GetStaticProps } from "next";
-import Head from "next/head";
-import Image from "next/image";
 import { InputText } from "@/components/InputText";
 import { InputTextarea } from "@/components/InputTextarea";
 import { ElemButton } from "@/components/ElemButton";
 import { IconPaperAirplane } from "@/components/Icons";
-
+import { FigurePerspectiveGrid } from "@/components/Figures";
 import { useFormspark } from "@formspark/use-formspark";
-
-import backgroundImage from "@/images/background-shapes1.jpg";
 
 const Contact: NextPage = () => {
 	const [submit, submitting] = useFormspark({
@@ -41,86 +37,74 @@ const Contact: NextPage = () => {
 	};
 
 	return (
-		<div>
-			<div>
-				<div className="relative overflow-hidden min-h-[70vh] bg-gray-50 py-20 sm:py-32">
-					<div className="absolute top-0 left-1/2 -translate-x-[30%] -translate-y-[5%]">
-						<Image
-							src={backgroundImage}
-							alt=""
-							width={1558}
-							height={946}
-							layout="fixed"
-							unoptimized
-						/>
-					</div>
-					<div className="relative z-10 7xl mx-auto px-4 sm:px-6 lg:px-8">
-						<div className="space-y-6 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-16 ">
-							<div>
-								<h1 className="relative max-w-3xl text-4xl lg:text-6xl font-bold">
-									Contact
-								</h1>
-								<p className="mt-4 text-xl text-slate-600">
-									Partner with EdgeIn, submit a profile, or simply get in touch.
-									Our team would love to hear from you!
-								</p>
+		<div className="lg:py-10 lg:min-h-[40vh] -mb-24 pb-36 bg-[url('/images/bg-shapes1.svg')] bg-cover bg-no-repeat bg-center">
+			<div className="max-w-2xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+				<h1 className="relative max-w-3xl text-4xl lg:text-6xl font-bold">
+					Contact
+				</h1>
+				<div className="mt-2 text-xl text-slate-600">
+					Partner with EdgeIn, submit a profile, or simply get in touch.
+					<div>Our team would love to hear from you!</div>
+				</div>
+				<div className="mt-16 relative">
+					<FigurePerspectiveGrid className="block absolute z-0 w-full scale-[2.5] bottom-0 opacity-30 text-dark-500" />
+					<div className="absolute -top-8 left-0 right-0 aspect-video w-10/12 mx-auto rounded-2xl bg-gradient-to-tr from-white/80 to-white/20 border-2 border-white/60 opacity-80 backdrop-blur-3xl"></div>
+					<div className="absolute -top-4 left-0 right-0 aspect-video w-11/12 mx-auto rounded-2xl bg-gradient-to-tr from-white/80 to-white/20 border-2 border-white/60 opacity-80 backdrop-blur-3xl"></div>
+
+					<div className="rounded-2xl bg-white p-6 relative z-10">
+						{formSent ? (
+							<div className="flex flex-col items-center justify-center sm:h-full">
+								<IconPaperAirplane className="mx-auto h-12 w-12 text-slate-300" />
+								<h2 className="text-2xl font-bold text-center mt-5 lg:text-3xl ">
+									Message Sent!
+								</h2>
 							</div>
-							<div className="relative rounded-2xl bg-white p-6 shadow-xl shadow-slate-900/10">
-								{formSent ? (
-									<div className="flex flex-col items-center justify-center sm:h-full">
-										<IconPaperAirplane className="mx-auto h-12 w-12 text-slate-300" />
-										<h2 className="text-2xl font-bold text-center mt-5 lg:text-3xl ">
-											Message Sent!
-										</h2>
+						) : (
+							<>
+								<form
+									className="relative grid grid-cols-1 gap-y-4 mt-6 sm:grid-cols-2 sm:gap-x-8"
+									onSubmit={onSubmit}
+								>
+									<div className="group mb-2 sm:col-span-2">
+										<InputText
+											label="Full Name"
+											type="text"
+											name="name"
+											value={name}
+											onChange={(e) => setName(e.target.value)}
+											placeholder="e.g. John Edge"
+											required
+										/>
 									</div>
-								) : (
-									<>
-										<form
-											className="relative grid grid-cols-1 gap-y-4 mt-6 sm:grid-cols-2 sm:gap-x-8"
-											onSubmit={onSubmit}
-										>
-											<div className="group mb-2 sm:col-span-2">
-												<InputText
-													label="Full Name"
-													type="text"
-													name="name"
-													value={name}
-													onChange={(e) => setName(e.target.value)}
-													placeholder="e.g. John Edge"
-													required
-												/>
-											</div>
-											<div className="group mb-2 sm:col-span-2">
-												<InputText
-													label="Work Email"
-													type="email"
-													name="email"
-													value={email}
-													onChange={(e) => setEmail(e.target.value)}
-													placeholder="example@email.com"
-													required
-												/>
-											</div>
-											<div className="group mb-2 sm:col-span-2">
-												<InputTextarea
-													label="Message"
-													name="message"
-													value={message}
-													rows={3}
-													onChange={(e) => setMessage(e.target.value)}
-													required
-												/>
-											</div>
-											<div className="text-right sm:col-span-2">
-												<ElemButton btn="primary" loading={submitting}>
-													Send
-												</ElemButton>
-											</div>
-										</form>
-									</>
-								)}
-							</div>
-						</div>
+									<div className="group mb-2 sm:col-span-2">
+										<InputText
+											label="Work Email"
+											type="email"
+											name="email"
+											value={email}
+											onChange={(e) => setEmail(e.target.value)}
+											placeholder="example@email.com"
+											required
+										/>
+									</div>
+									<div className="group mb-2 sm:col-span-2">
+										<InputTextarea
+											label="Message"
+											name="message"
+											value={message}
+											rows={3}
+											onChange={(e) => setMessage(e.target.value)}
+											required
+										/>
+									</div>
+									<div className="text-right sm:col-span-2">
+										<ElemButton btn="primary" loading={submitting}>
+											Send
+										</ElemButton>
+									</div>
+								</form>
+							</>
+						)}
 					</div>
 				</div>
 			</div>
