47c172ffc8bfc29ae3eadf9d61f629cb1c379847
Sume <rainmakers.development12@rainmakers.org>
2022-09-13 11:18:28 +0530

    Add dashboard layout to account page

--- pages/account.tsx
+++ pages/account.tsx
@@ -14,6 +14,7 @@ import { InputTextarea } from "@/components/InputTextarea";
 import { InputSelect } from "@/components/InputSelect";
 import person from "../images/person.png"
 import { IconSetting } from "@/components/IconSetting";
+import { DashboardLayout } from '@/components/Dashboard/DashboardLayout';
 const validator = require("validator");
 
 export default function Account() {
@@ -93,82 +94,9 @@ export default function Account() {
     return (
         <div className="max-w-6xl px-4 pt-4 mx-auto sm:px-6 lg:px-8 lg:pt-10 mt-10">
             <div className="grid grid-cols-4 gap-4">
-                <div className="col-span-1">
-                    {/* my edgein */}
-
-                    <div className="mb-4">
-                        <h3 className="text-xl font-bold py-1  text-dark-500">My EdegIn</h3>
-                        <ul className="flex flex-col">
-                            <li
-                                className="py-2 text-slate-600 inline-flex items-center"
-                                role="button"
-                            >
-                                <Link href=""
-                                >
-                                    <a className="inline-flex items-center">
-                                        <Image src={person} alt="person img" /><span className="ml-2">Bram Cohen</span>
-                                    </a>
-                                </Link>
-                            </li>
-                            <li
-                                className="py-2 relative right-2 w-60 text-slate-600 inline-flex items-center bg-slate-200 px-2 rounded-lg"
-                                role="button">
-                                <Link href=""
-                                >
-                                    <a className="inline-flex items-center">
-                                        <IconSetting /> <span className="ml-2 font-normal font-Metropolis text-md">Account Setting</span>
-                                    </a>
-                                </Link>
-                            </li>
-
-
-                            {/* {renderMyCustomList()} */}
-                        </ul>
-                    </div>
-
-                    {/* my list */}
-                    <h3 className="text-xl font-bold py-1 text-dark-500">My List</h3>
-                    <ul className="flex flex-col">
-                        <li
-                            className="py-2 text-slate-600 inline-flex items-center"
-                            role="button"
-                        >
-                            <Link href="" //href={`/lists/${hotId}`}
-                            >
-                                <a className="inline-flex items-center">
-                                    <IconHot className="mr-1 w-7" /> Hot
-                                </a>
-                            </Link>
-                        </li>
-                        <li
-                            className="py-2 text-slate-600 inline-flex items-center"
-                            role="button">
-                            <Link href="" //href={`/lists/${likeId}`}
-                            >
-                                <a className="inline-flex items-center">
-                                    <IconLike className="mr-1 w-7" /> Like
-                                </a>
-                            </Link>
-                        </li>
-                        <li
-                            className={`py-2 text-slate-600 inline-flex items-center`}
-                            role="button"
-                        >
-                            <Link href="" //href={`/lists/${crapId}`}
-                            >
-                                <a className="inline-flex items-center">
-                                    <IconCrap className="mr-1 w-7" /> Crap
-
-                                </a>
-                            </Link>
-                        </li>
-
-                        {/* {renderMyCustomList()} */}
-                    </ul>
-                </div>
-
+               <DashboardLayout />
                 <div className="col-span-3">
-                    <div className="max-w-6xl bg-white rounded-lg p-5">
+                    <div className="bg-white rounded-lg p-5">
                         <div className="flex justify-between items-center mb-4">
                             <h2 className="text-dark-500 font-bold text-xl">Account Setting</h2>
                         </div>
