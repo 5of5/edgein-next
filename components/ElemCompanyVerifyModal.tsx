434e57bce542ff6fd1bfce24bcc81751a98e4404
Sagar Guhe <rainmakers.development11@rainmakers.org>
2022-09-20 17:10:14 +0530

    fix ElemCompanyVeifyModal

--- components/ElemCompanyVerifyModal.tsx
+++ components/ElemCompanyVerifyModal.tsx
@@ -110,7 +110,7 @@ export const ElemCompanyVerifyModal: React.FC<Props> = ({ isOpen, onClose, dropd
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="w-full max-w-xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Panel className="w-full max-w-xl transform overflow-y-auto rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                   {!isEmailEntered ?
                     <>
                       <Dialog.Title
