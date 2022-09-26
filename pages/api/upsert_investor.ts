83c9501225de98bf615f2f7751fdc99321f3d053
Sagar Guhe <rainmakers.development11@rainmakers.org>
2022-09-26 11:58:56 +0530

    add upsert_investor api, fix update_vc_firm api

--- pages/api/upsert_investor.ts
+++ pages/api/upsert_investor.ts
@@ -11,13 +11,13 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const user = await CookieService.getUser(token);
   if (!user) return res.status(403).end()
 
-  const data = req.body.vcFirm
+  const data = req.body.investor
   const vcFirmId = req.body.vcFirmId
 
   if (!vcFirmId) return res.status(400).send({ message: 'Bad request, vcFirmId required!' })
 
   try {
-    const result = await updateVcFirm(data, vcFirmId, token)
+    const result = await upsertInvestor(data, vcFirmId, token)
     res.send({ result })
   } catch (e: any) {
     return res.status(500).send({ message: 'some error occurred while saving organization' })
@@ -25,11 +25,18 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
 }
 
-const updateVcFirm = async (payload: any, vcFirmId: number, token: string) => {
+const upsertInvestor = async (payload: any, vcFirmId: number, token: string) => {
   const mutation = `
-    mutation UpdateVcFirmByPk($vcFirmId: Int!, $data: companies_set_input) {
-      update_vc_firms_by_pk(pk_columns: {id: $vcFirmId}, _set: $data) {
+    mutation UpsertInvestors($data: investors_insert_input!) {
+      insert_investors_one(object: $data, on_conflict: {constraint: investors_vc_firm_id_person_id_key, update_columns: [start_date, end_date, seniority, function, title]}) {
         id
+        person_id
+        vc_firm_id
+        title
+        seniority
+        start_date
+        end_date
+        created_at
       }
     }
   `
