0444e690e13c649d2379af5a80c11c7128f2a5be
Ed Parsons <ed@acutulus.co>
2022-11-10 22:08:32 -0800

    Adding tracking api

---
+++ pages/api/track.ts
@@ -0,0 +1,44 @@
+import { NextApiResponse, NextApiRequest } from "next";
+import { mutate } from '@/graphql/hasuraAdmin'
+import CookieService from '../../utils/cookie'
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== "GET") {
+    res.status(405).json({ message: "Method not allowed" });
+  }
+
+  // params:
+  const resourceId: string = req.body.resourceId
+  const resourceType: string = req.body.resourceType
+  const pathname: string = req.body.pathname
+  
+  const token = CookieService.getAuthToken(req.cookies)
+  const user = await CookieService.getUser(token);
+  if (!user) return res.status(403).end()
+
+    // create action
+    mutate({
+      mutation: `
+        mutation InsertAction($object: actions_insert_input!) {
+          insert_actions_one(
+            object: $object
+          ) {
+            id
+          }
+        }
+      `,
+      variables: {
+        object: {
+          action: "View",
+          page: pathname,
+          properties: {
+          },
+          resource_id: resourceId,
+          resource: resourceType,
+          user: user.id,
+        },
+      },
+    });
+}
+
+export default handler;
