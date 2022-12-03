dd9b923b1ea2f37324a1c5d07789e5e031ae5f14
Ed Parsons <ed@acutulus.co>
2022-12-02 20:25:37 -0800

    adding exception for submit_data

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -28,6 +28,7 @@ export async function middleware(req: NextRequest) {
 			`/api/change_password/`,
 			`/api/access_token_from_code/`,
 			`/api/stripe_webhook/`,
+			`/api/submit_data/`,
 			`/admin/app/`,
 		].includes(url.pathname) ||
 		url.pathname.endsWith(".png") ||
