a1bd08ca848b35b34b743283aa5bd93cc8f1fb4c
nhale101 <nha@techlist.com>
2022-12-20 14:09:18 +0700

    update api groups

--- pages/api/groups.ts
+++ pages/api/groups.ts
@@ -3,21 +3,44 @@ import GroupService from "@/utils/groups";
 import CookieService from "../../utils/cookie";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  // Create a user group
-  if (req.method !== "POST") {
-    res.status(405).json({ message: "Method not allowed" });
-  }
-
   const token = CookieService.getAuthToken(req.cookies);
   const user = await CookieService.getUser(token);
   if (!user) return res.status(403).end();
 
-  // params:
+  const id = req.body.id;
   const name: string = req.body.name;
 
-  // create action
-  const data = await GroupService.onInsertGroup(name);
-  res.send(data);
+  switch (req.method) {
+    case "POST": {
+      const data = await GroupService.onInsertGroup(name);
+      res.send(data);
+    }
+
+    case "PUT": {
+      // Update a group
+      const updatedGroup = await GroupService.onUpdateGroup(id, name);
+      return res.send(updatedGroup);
+    }
+
+    case "DELETE": {
+      // Delete invites of group
+      await GroupService.onDeleteGroupInvites(id);
+
+      // Delete members of group
+      await GroupService.onDeleteGroupMembers(id);
+
+      // Delete notes of group
+      await GroupService.onDeleteNotesByGroupId(id);
+
+      // Delete a group
+      const deletedGroup = await GroupService.onDeleteGroup(id);
+
+      return res.send(deletedGroup);
+    }
+
+    default:
+      return res.status(405).json({ message: "Method not allowed" });
+  }
 };
 
 export default handler;
