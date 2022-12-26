383e739f58c9761f06c5bbd4138ad5dd61a443b0
nhale101 <nha@techlist.com>
2022-12-26 16:53:19 +0700

    add trigger updated_at on notes table

--- infra/hasura/migrations/default/1672040323251_feature_groups_and_notes/up.sql
+++ infra/hasura/migrations/default/1672040323251_feature_groups_and_notes/up.sql
@@ -30,3 +30,20 @@ FOR EACH ROW
 EXECUTE PROCEDURE "public"."set_current_timestamp_updated_at"();
 COMMENT ON TRIGGER "set_public_user_groups_updated_at" ON "public"."user_groups" 
 IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+
+CREATE OR REPLACE FUNCTION "public"."set_current_timestamp_updated_at"()
+RETURNS TRIGGER AS $$
+DECLARE
+  _new record;
+BEGIN
+  _new := NEW;
+  _new."updated_at" = NOW();
+  RETURN _new;
+END;
+$$ LANGUAGE plpgsql;
+CREATE TRIGGER "set_public_notes_updated_at"
+BEFORE UPDATE ON "public"."notes"
+FOR EACH ROW
+EXECUTE PROCEDURE "public"."set_current_timestamp_updated_at"();
+COMMENT ON TRIGGER "set_public_notes_updated_at" ON "public"."notes" 
+IS 'trigger to set value of column "updated_at" to current timestamp on row update';
