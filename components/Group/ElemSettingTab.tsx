975ed7c45c3b34b56ad61bc508bc593433e91463
nhale101 <nha@techlist.com>
2023-01-09 18:07:58 +0700

    action leave group

--- components/Group/ElemSettingTab.tsx
+++ components/Group/ElemSettingTab.tsx
@@ -1,8 +1,9 @@
-import { IconSignOut, IconTrash } from "@/components/Icons";
-import { useUser } from "@/context/userContext";
-import { User_Groups } from "@/graphql/types";
+import { useState } from "react";
 import { useRouter } from "next/router";
 import { useMutation } from "react-query";
+import { useUser } from "@/context/userContext";
+import { User_Groups } from "@/graphql/types";
+import { IconSignOut, IconTrash, IconX } from "@/components/Icons";
 import ElemSettingEditableField from "./ElemSettingEditableField";
 
 type Props = {
@@ -13,7 +14,9 @@ type Props = {
 const ElemSettingTab: React.FC<Props> = ({ group, onUpdateGroupData }) => {
 	const router = useRouter();
 
-	const { refetchMyGroups } = useUser();
+	const { user, refetchMyGroups } = useUser();
+
+	const [leaveError, setLeaveError] = useState<boolean>(false);
 
 	const fields = [
 		{
@@ -38,7 +41,7 @@ const ElemSettingTab: React.FC<Props> = ({ group, onUpdateGroupData }) => {
 		},
 	]
 
-	const { mutate } = useMutation(
+	const { mutate: deleteGroup } = useMutation(
 		() =>
 			fetch("/api/groups/", {
 				method: "DELETE",
@@ -57,8 +60,37 @@ const ElemSettingTab: React.FC<Props> = ({ group, onUpdateGroupData }) => {
 	);
 
 	const handleDelete = () => {
-		mutate();
+		deleteGroup();
 	};
+
+	const { mutate: leaveGroup } = useMutation(
+    (memberId: number | undefined) =>
+      fetch("/api/delete_group_member/", {
+        method: "DELETE",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id: memberId,
+        }),
+      }),
+    {
+      onSuccess: () => {
+        refetchMyGroups();
+				router.push("/account");
+      },
+    }
+  );
+
+	const handleLeaveGroup = () => {
+		if (user?.id === group.created_by_user_id) {
+			setLeaveError(true);
+		} else {
+			const memberId = group.user_group_members.find(mem => mem.user.id === user?.id);
+			leaveGroup(memberId?.id);
+		}
+	}
 
 	return (
 		<>
@@ -74,12 +106,25 @@ const ElemSettingTab: React.FC<Props> = ({ group, onUpdateGroupData }) => {
 					/>
 				))}
 
-				<div
-					className="flex items-start px-4 py-3 cursor-pointer space-x-1 hover:bg-slate-100"
-					onClick={() => {}}
-				>
-					<IconSignOut className="w-6 h-6 text-red-500" />
-					<p className="font-bold text-red-500">Leave Group</p>
+				<div>
+					<div
+						className="flex items-start space-x-1 px-4 py-3 cursor-pointer hover:bg-slate-100"
+						onClick={handleLeaveGroup}
+					>
+						<IconSignOut className="w-6 h-6 text-red-500" />
+						<p className="font-bold text-red-500">Leave Group</p>
+					</div>
+					{leaveError && (
+						<div className="flex justify-between px-4 pb-3 text-red-600 text-sm">
+							<div>
+								<p>You cannot leave the group when you are the group&apos;s manager.</p>
+								<p>Please make another member as group manager then leave group or you can delete group.</p>
+							</div>
+							<span onClick={() => setLeaveError(false)}>
+								<IconX className="w-4 h-4 text-slate-700 cursor-pointer" />
+							</span>
+						</div>
+					)}
 				</div>
 			</div>
 
