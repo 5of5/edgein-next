3dab5b8e1f4b001710be7e7dcade15cfe2bc10f0
raylopro <contact@raylopro.com>
2023-01-29 17:13:28 -0800

    icons fix

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -1580,30 +1580,6 @@ export const IconTable: React.FC<IconProps> = ({
 	);
 };
 
-export const IconColumns: React.FC<IconProps> = ({
-	className,
-	strokeWidth,
-	title,
-}) => {
-	return (
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			className={className}
-			viewBox="0 0 24 24"
-			fill="none"
-			strokeWidth={strokeWidth ? strokeWidth : 1.5}
-			stroke="currentColor"
-		>
-			<title>{title ? title : "columns"}</title>
-			<path
-				strokeLinecap="round"
-				strokeLinejoin="round"
-				d="M9 4.5v15m6-15v15m-10.875 0h15.75c.621 0 1.125-.504 1.125-1.125V5.625c0-.621-.504-1.125-1.125-1.125H4.125C3.504 4.5 3 5.004 3 5.625v12.75c0 .621.504 1.125 1.125 1.125z"
-			/>
-		</svg>
-	);
-};
-
 export const IconShare: React.FC<IconProps> = ({
 	className,
 	strokeWidth,
