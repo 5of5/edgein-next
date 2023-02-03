9e87208fa5437fd8064f21c9ba3204fb03b0f3f4
raylopro <contact@raylopro.com>
2023-02-02 18:41:27 -0800

    tabs fix

--- components/ElemTabBar.tsx
+++ components/ElemTabBar.tsx
@@ -15,9 +15,14 @@ type Tabs = {
 type Props = {
 	className?: string;
 	tabs?: Array<Tabs>;
+	showDropdown?: boolean;
 };
 
-export const ElemTabBar: React.FC<Props> = ({ className, tabs }) => {
+export const ElemTabBar: React.FC<Props> = ({
+	className,
+	tabs,
+	showDropdown = true,
+}) => {
 	const [isActive, setActive] = useState(0);
 
 	const onClick = (index: number, ref: any) => {
@@ -48,32 +53,33 @@ export const ElemTabBar: React.FC<Props> = ({ className, tabs }) => {
 						</button>
 					))}
 			</nav>
+			{showDropdown && (
+				<Popover className="relative z-10">
+					<Popover.Button className="relative inline-flex items-center text-sm rounded-md px-2 py-1.5 transition ease-in-out duration-150 group bg-white ring-inset ring-1 ring-slate-200 hover:text-primary-500 hover:bg-slate-200 focus:outline-none focus:ring-1">
+						<IconEllipsisHorizontal
+							className="h-6 w-6 group-hover:text-primary-500"
+							title="Options"
+						/>
+					</Popover.Button>
 
-			<Popover className="relative z-10">
-				<Popover.Button className="relative inline-flex items-center text-sm rounded-md px-2 py-1.5 transition ease-in-out duration-150 group bg-white ring-inset ring-1 ring-slate-200 hover:text-primary-500 hover:bg-slate-200 focus:outline-none focus:ring-1">
-					<IconEllipsisHorizontal
-						className="h-6 w-6 group-hover:text-primary-500"
-						title="Options"
-					/>
-				</Popover.Button>
-
-				<Popover.Panel className="absolute right-0 overflow-hidden w-48 divide-y divide-slate-100 rounded-lg bg-white shadow-lg ring-1 ring-black/5">
-					<button
-						onClick={show}
-						className="flex items-center space-x-2 w-full px-2 py-2 hover:bg-gray-50 hover:text-primary-500"
-					>
-						<IconPencilSquare className="h-6 w-6 group-hover:text-primary-500" />
-						<span>Suggest edits</span>
-					</button>
-					<button
-						onClick={show}
-						className="flex items-center space-x-2 w-full px-2 py-2 hover:bg-gray-50 hover:text-primary-500"
-					>
-						<IconExclamationTriangle className="h-6 w-6 group-hover:text-primary-500" />
-						<span>Report an error</span>
-					</button>
-				</Popover.Panel>
-			</Popover>
+					<Popover.Panel className="absolute right-0 overflow-hidden w-48 divide-y divide-slate-100 rounded-lg bg-white shadow-lg ring-1 ring-black/5">
+						<button
+							onClick={show}
+							className="flex items-center space-x-2 w-full px-2 py-2 hover:bg-gray-50 hover:text-primary-500"
+						>
+							<IconPencilSquare className="h-6 w-6 group-hover:text-primary-500" />
+							<span>Suggest edits</span>
+						</button>
+						<button
+							onClick={show}
+							className="flex items-center space-x-2 w-full px-2 py-2 hover:bg-gray-50 hover:text-primary-500"
+						>
+							<IconExclamationTriangle className="h-6 w-6 group-hover:text-primary-500" />
+							<span>Report an error</span>
+						</button>
+					</Popover.Panel>
+				</Popover>
+			)}
 		</div>
 	);
 };
