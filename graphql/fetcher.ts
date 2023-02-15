b76d536849cd0a76112a5d41fd06c12e71ee1ced
Ed Parsons <ed@acutulus.co>
2023-02-15 09:09:32 -0800

    Fixing fetcher error handling

--- graphql/fetcher.ts
+++ graphql/fetcher.ts
@@ -1,19 +1,30 @@
 
 export function fetcher<TData, TVariables>(query: string, variables?: TVariables, options?: HeadersInit | undefined) {
   return async (): Promise<TData> => {
-    const res = await fetch("/api/graphql/", {
-    method: "POST",
-      body: JSON.stringify({ query, variables }),
-    });
+    try {
+      const res = await fetch("/api/graphql/", {
+        method: "POST",
+          body: JSON.stringify({ query, variables }),
+        });
+        if (!res.ok) {
+          if (res.redirected && res.url) {
+            location.href = res.url;
+            throw new Error("Redirect");
+          } else {
+            throw new Error(res.statusText);
+          }
+        }
+        const json = await res.json();
 
-    const json = await res.json();
-
-    if (json.errors) {
-      const { message } = json.errors[0];
-
-      throw new Error(message);
+        if (json.errors) {
+          const { message } = json.errors[0];
+    
+          throw new Error(message);
+        }
+    
+        return json.data;    
+      } catch (e: any) {
+        throw new Error(e['message']);
     }
-
-    return json.data;
   }
 }
