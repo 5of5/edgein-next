98098e4cae82081e70b710fd36760dee794fbf1c
raylopro <contact@raylopro.com>
2023-03-14 23:05:00 -0700

    fix notifications upgrade

--- pages/notifications.tsx
+++ pages/notifications.tsx
@@ -27,10 +27,9 @@ const Notifications: NextPage = () => {
 
 	const displayedNotifications = notifications?.slice(
 		0,
-		notifications?.length
-		// user?.entitlements.listsCount
-		// 	? user?.entitlements.listsCount
-		// 	: notifications?.length
+		user?.entitlements.listsCount
+			? user?.entitlements.listsCount
+			: notifications?.length
 	);
 
 	const [notificationsLimit, setNotificationsLimit] = useState(4);
