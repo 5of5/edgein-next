661c36330ac0b11ae0f9520fc434bd2e435560fa
nhale101 <nha@techlist.com>
2023-03-16 14:04:59 +0700

    send email reset password

--- components/admin/user/UserResetPasswordButton.tsx
+++ components/admin/user/UserResetPasswordButton.tsx
@@ -1,24 +1,42 @@
 import { useState } from "react";
-import { Button, Confirm, useRecordContext } from "react-admin";
+import { Button, Confirm, useRecordContext, useNotify } from "react-admin";
+import { useMutation } from "react-query";
 
 const UserResetPasswordButton = () => {
   const record = useRecordContext();
+  const notify = useNotify();
+
   const [open, setOpen] = useState(false);
 
   const handleClick = () => setOpen(true);
 
   const handleDialogClose = () => setOpen(false);
 
-  const handleConfirm = async () => {
-    await fetch("/api/change_password/", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
+  const { mutate, isLoading } = useMutation(
+    () =>
+      fetch("/api/change_password/", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email: record.email }),
+      }),
+    {
+      onSuccess: async (response) => {
+        if (response.status !== 200) {
+          const err = await response.json();
+          notify(err.message, { type: "error" });
+        } else {
+          notify(`An email has been sent to ${record.email}`, { type: "info" });
+        }
+        handleDialogClose();
       },
-      body: JSON.stringify({ email: record.email }),
-    });
-    setOpen(false);
+    }
+  );
+
+  const handleConfirm = () => {
+    mutate();
   };
 
   return (
@@ -30,6 +48,7 @@ const UserResetPasswordButton = () => {
       />
       <Confirm
         isOpen={open}
+        loading={isLoading}
         title={`Reset password user ${record?.display_name}`}
         content="Are you sure you want to reset this user's password?"
         onConfirm={handleConfirm}
