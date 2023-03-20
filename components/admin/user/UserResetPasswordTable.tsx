50aab92e30d35aee068e7ba302c383d635c5ed5b
nhale101 <nha@techlist.com>
2023-03-20 16:11:09 +0700

    update style

--- components/admin/user/UserResetPasswordTable.tsx
+++ components/admin/user/UserResetPasswordTable.tsx
@@ -33,55 +33,61 @@ const UserResetPasswordTable = () => {
     filter: { user_id: parseInt(currentId!) },
   });
 
-  return (
-    <List
-      pagination={false}
-      actions={<ListActions />}
-      sx={{
-        ".MuiToolbar-root": {
-          justifyContent: "start !important",
-          paddingTop: 0,
-          marginBottom: "4px",
-        },
-        ".RaBulkActionsToolbar-toolbar": {
-          justifyContent: "start !important",
-        },
-        ".MuiToolbar-root .MuiButtonBase-root": {
-          paddingTop: 0,
-          paddingBottom: 0,
-          margin: "4px",
-        },
-        ".RaBulkActionsToolbar-topToolbar": {
-          paddingTop: 0,
-          paddingBottom: 0,
-          marginBottom: 0,
-        },
-        ".MuiToolbar-root form": {
-          flex: "0 1 auto",
-        },
-        ".MuiToolbar-root form .MuiFormControl-root": {
-          margin: 0,
-        },
-      }}
-    >
-      <Datagrid bulkActionButtons={false} data={data}>
-        <TextField source="id" />
-        <FunctionField
-          label="Generated password"
-          render={(record: any) => <ElemGeneratedPassword value={record.generated_password} />}
-        />
-        <ReferenceField
-          label="Created by"
-          source="created_by_user_id"
-          reference="users"
-          sortable={false}
-        >
-          <TextField source="display_name" />
-        </ReferenceField>
-        <DateField source="created_at" showTime />
-      </Datagrid>
-    </List>
-  );
+  if (data && data.length > 0) {
+    return (
+      <List
+        pagination={false}
+        actions={<ListActions />}
+        sx={{
+          ".MuiToolbar-root": {
+            justifyContent: "start !important",
+            paddingTop: 0,
+            marginBottom: "4px",
+          },
+          ".RaBulkActionsToolbar-toolbar": {
+            justifyContent: "start !important",
+          },
+          ".MuiToolbar-root .MuiButtonBase-root": {
+            paddingTop: 0,
+            paddingBottom: 0,
+            margin: "4px",
+          },
+          ".RaBulkActionsToolbar-topToolbar": {
+            paddingTop: 0,
+            paddingBottom: 0,
+            marginBottom: 0,
+          },
+          ".MuiToolbar-root form": {
+            flex: "0 1 auto",
+          },
+          ".MuiToolbar-root form .MuiFormControl-root": {
+            margin: 0,
+          },
+        }}
+      >
+        <Datagrid bulkActionButtons={false} data={data}>
+          <TextField source="id" />
+          <FunctionField
+            label="Generated password"
+            render={(record: any) => (
+              <ElemGeneratedPassword value={record.generated_password} />
+            )}
+          />
+          <ReferenceField
+            label="Created by"
+            source="created_by_user_id"
+            reference="users"
+            sortable={false}
+          >
+            <TextField source="display_name" />
+          </ReferenceField>
+          <DateField source="created_at" showTime />
+        </Datagrid>
+      </List>
+    );
+  }
+
+  return null;
 };
 
 export default UserResetPasswordTable;
