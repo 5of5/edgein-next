196c4d2742f7b7046c5cd019eda77e5ea9f8d876
raylopro <contact@raylopro.com>
2023-03-27 18:07:43 -0700

    notification alerts

--- components/NotificationAlerts.tsx
+++ components/NotificationAlerts.tsx
@@ -1,23 +1,17 @@
-import { ElemButton } from "./ElemButton";
-import { IconAlert, IconAlertSolid } from "@/components/Icons";
+import { IconBell } from "@/components/Icons";
+import Link from "next/link";
 
 export const NotificationAlerts = () => {
 	const notifications = [];
 
 	return (
-		<ElemButton
-			btn="slate"
-			href="/notifications"
-			className="h-9 w-9 !px-0 !py-0 group"
-		>
-			{notifications.length > 0 ? (
-				<IconAlertSolid
-					className="h-6 w-6 text-primary-500"
-					aria-hidden="true"
-				/>
-			) : (
-				<IconAlert className="h-6 w-6" aria-hidden="true" />
-			)}
-		</ElemButton>
+		<Link href="/notifications" passHref>
+			<a className="relative flex items-center group-hover:opacity-50 hover:!opacity-100">
+				{notifications.length > 0 && (
+					<div className="absolute -top-[6px] -right-[6px] w-4 h-4 rounded-full from-blue-800 via-primary-500 to-primary-400 bg-gradient-to-r border-2 border-white"></div>
+				)}
+				<IconBell className="h-5 w-5" strokeWidth={2} />
+			</a>
+		</Link>
 	);
 };
