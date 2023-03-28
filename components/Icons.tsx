975445a003e7c4cea52dbc8c7da876fc82db3dec
raylopro <contact@raylopro.com>
2023-03-27 18:08:03 -0700

    alerts fix

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -1193,6 +1193,28 @@ export const IconGlobe: React.FC<IconProps> = ({
 	);
 };
 
+export const IconBell: React.FC<IconProps> = ({
+	className,
+	title,
+	strokeWidth,
+}) => {
+	return (
+		<svg
+			className={className}
+			fill="none"
+			viewBox="0 0 24 24"
+			aria-hidden="true"
+			strokeWidth={strokeWidth ? strokeWidth : 1.5}
+			stroke="currentColor"
+			strokeLinecap="round"
+		>
+			<title>{title ? title : "Notification"}</title>
+			<path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
+			<path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
+		</svg>
+	);
+};
+
 export const IconEmail: React.FC<IconProps> = ({
 	className,
 	title,
