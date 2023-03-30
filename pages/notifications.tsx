52dc6690f94bb892d9dea943ce6a34933f937ec1
raylopro <contact@raylopro.com>
2023-03-29 21:21:58 -0700

    fix notifications length

--- pages/notifications.tsx
+++ pages/notifications.tsx
@@ -48,10 +48,9 @@ const Notifications: NextPage = () => {
 
 	const displayedNotifications = notifications?.slice(
 		0,
-		notifications?.length
-		// user?.entitlements.listsCount
-		// 	? user?.entitlements.listsCount
-		// 	: notifications?.length
+		user?.entitlements.listsCount
+			? user?.entitlements.listsCount
+			: notifications?.length
 	);
 
 	const [notificationsLimit, setNotificationsLimit] = useState(5);
