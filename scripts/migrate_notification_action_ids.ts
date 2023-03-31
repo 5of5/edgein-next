2cb8893cbec782aa833b5f0ac6b8fc70e6d4f579
nhale101 <nha@techlist.com>
2023-03-31 10:02:17 +0700

    update script migrate notification actions

--- scripts/migrate_notification_action_ids.ts
+++ scripts/migrate_notification_action_ids.ts
@@ -11,15 +11,27 @@ import { getClient } from "./postgres_helpers";
 
   for (let i = 0; i < queryResults.rows.length; i += 1) {
     const record = queryResults.rows[i];
-    console.log(`Migrate action_ids for notification #${record.id}`);
 
     const actionIds = record.action_ids;
 
     for (let j = 0; j < actionIds.length; j += 1) {
       if (actionIds[j]) {
-        await client.query(
-          `INSERT INTO notification_actions (notification_id, action_id) VALUES(${record.id}, ${actionIds[j]})`
-        );
+        const existedRecords = await client.query(`
+          SELECT id
+          FROM notification_actions
+          WHERE (notification_id = ${record.id} AND action_id = ${actionIds[j]})
+          LIMIT 1
+        `);
+        if (existedRecords.rows.length === 0) {
+          console.log(
+            `Migrate action_ids for notification #${record.id} with action #${actionIds[j]}`
+          );
+
+          await client.query(`
+            INSERT INTO notification_actions (notification_id, action_id)
+            VALUES(${record.id}, ${actionIds[j]})
+          `);
+        }
       }
     }
   }
