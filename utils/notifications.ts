47c2b3999934ed1e6f9a89bdca09ba65afa77972
raylopro <contact@raylopro.com>
2023-04-08 09:18:08 -0700

    notification text update

--- utils/notifications.ts
+++ utils/notifications.ts
@@ -99,7 +99,7 @@ const getMessageContents = (
 		return "added new key info";
 	} else {
 		if (notificationResourceType === "team_members") {
-			return "deleted a team info";
+			return "deleted team info";
 		} else if (notificationResourceType === "investments") {
 			return "deleted investments data";
 		} else if (notificationResourceType === "investment_rounds") {
