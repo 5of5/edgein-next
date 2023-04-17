a3e5727dfa229c6c580ae5bd025b6ff87aef8545
raylopro <contact@raylopro.com>
2023-04-16 23:08:23 -0700

    link account to profile modal update

--- components/Event/ElemRequireLinkPersonDialog.tsx
+++ components/Event/ElemRequireLinkPersonDialog.tsx
@@ -5,79 +5,79 @@ import { ElemLogo } from "@/components/ElemLogo";
 import { ElemButton } from "@/components/ElemButton";
 
 type Props = {
-  isOpen: boolean;
-  onClose: () => void;
-  onClickSearch: () => void;
+	isOpen: boolean;
+	onClose: () => void;
+	onClickSearch: () => void;
 };
 
 export const ElemRequireLinkPersonDialog: FC<Props> = ({
-  isOpen,
-  onClose,
-  onClickSearch,
+	isOpen,
+	onClose,
+	onClickSearch,
 }) => {
-  return (
-    <Transition appear show={isOpen} as={Fragment}>
-      <Dialog as="div" className="relative z-40" onClose={onClose}>
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="fixed inset-0 bg-black bg-opacity-25" />
-        </Transition.Child>
+	return (
+		<Transition appear show={isOpen} as={Fragment}>
+			<Dialog as="div" className="relative z-40" onClose={onClose}>
+				<Transition.Child
+					as={Fragment}
+					enter="ease-out duration-300"
+					enterFrom="opacity-0"
+					enterTo="opacity-100"
+					leave="ease-in duration-200"
+					leaveFrom="opacity-100"
+					leaveTo="opacity-0"
+				>
+					<div className="fixed inset-0 bg-black bg-opacity-25" />
+				</Transition.Child>
 
-        <div className="fixed inset-0 overflow-y-auto">
-          <div className="flex min-h-full items-center justify-center p-4 text-center">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
-            >
-              <Dialog.Panel className="w-full max-w-xl transform rounded-lg bg-slate-100 p-6 shadow-xl transition-all">
-                <div className="flex justify-end items-center">
-                  <button
-                    type="button"
-                    onClick={onClose}
-                    className="focus-visible:outline-none"
-                  >
-                    <IconX className="h-6 w-6" />
-                  </button>
-                </div>
-                <div className="flex items-center h-12 w-12 p-2 mx-auto rounded-full shadow">
-                  <ElemLogo mode="icon" className="w-10 aspect-square" />
-                </div>
+				<div className="fixed inset-0 overflow-y-auto">
+					<div className="flex min-h-full items-center justify-center p-4 text-center">
+						<Transition.Child
+							as={Fragment}
+							enter="ease-out duration-300"
+							enterFrom="opacity-0 scale-95"
+							enterTo="opacity-100 scale-100"
+							leave="ease-in duration-200"
+							leaveFrom="opacity-100 scale-100"
+							leaveTo="opacity-0 scale-95"
+						>
+							<Dialog.Panel className="w-full max-w-xl transform rounded-lg bg-slate-100 p-6 shadow-xl transition-all">
+								<div className="flex justify-end items-center">
+									<button
+										type="button"
+										onClick={onClose}
+										className="focus-visible:outline-none"
+									>
+										<IconX className="h-6 w-6" />
+									</button>
+								</div>
+								<div className="flex items-center h-12 w-12 p-2 mx-auto rounded-full shadow">
+									<ElemLogo mode="icon" className="w-10 aspect-square" />
+								</div>
 
-                <Dialog.Title className="mt-4 text-2xl text-center font-bold lg:text-3xl">
-                  You have not linked your account to a profile on EdgeIn
-                </Dialog.Title>
+								<Dialog.Title className="mt-4 text-2xl text-center font-bold lg:text-3xl">
+									You have not linked your account to a profile on EdgeIn
+								</Dialog.Title>
 
-                <div className="mt-4 text-slate-600">
-                  <p className="text-slate-600">
-                    Search your name on the site and claim profile to be able to
-                    going this event.
-                  </p>
-                </div>
+								<div className="mt-4 text-slate-600">
+									<p className="text-slate-600">
+										Search your name and claim profile to be able to mark
+										yourself as going to this event.
+									</p>
+								</div>
 
-                <ElemButton
-                  onClick={onClickSearch}
-                  btn="primary"
-                  className="mx-auto mt-6"
-                >
-                  Search name
-                </ElemButton>
-              </Dialog.Panel>
-            </Transition.Child>
-          </div>
-        </div>
-      </Dialog>
-    </Transition>
-  );
+								<ElemButton
+									onClick={onClickSearch}
+									btn="primary"
+									className="mx-auto mt-6"
+								>
+									Search name
+								</ElemButton>
+							</Dialog.Panel>
+						</Transition.Child>
+					</div>
+				</div>
+			</Dialog>
+		</Transition>
+	);
 };
