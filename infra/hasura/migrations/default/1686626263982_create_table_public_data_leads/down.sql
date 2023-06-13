c0322561a75d2f93c329ecb8f593b67c1032f286
Nam <nam@techlist.com>
2023-06-13 10:21:11 +0700

    Add leads table for instantly.ai

---
+++ infra/hasura/migrations/default/1686626263982_create_table_public_data_leads/down.sql
@@ -0,0 +1,21 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- CREATE TABLE "public"."leads" (
+-- "id" serial NOT NULL,
+-- "first_name" text NULL,
+-- "last_name" text NULL,
+-- "email" text NOT NULL,
+-- "phone" text NULL,
+-- "company_name" text NULL,
+-- "website" text NULL,
+-- "status" text NOT NULL DEFAULT 'draft'::text,
+-- "created_at" date NOT NULL DEFAULT now(),
+-- "updated_at" date NOT NULL DEFAULT now(),
+-- PRIMARY KEY ("id"),
+-- UNIQUE ("email")
+-- );
+--
+-- CREATE trigger set_public_leads_updated_at before
+-- UPDATE
+--     ON
+--     public.leads for each row execute function set_current_timestamp_updated_at();
