acd5fb0388c6520562f5fc1d4f2002b373f480f0
Richard Dominik <richarddominik@sudolabs.io>
2023-06-16 10:51:17 +0200

    Add script to migrate companies key_info jsonb column

---
+++ scripts/migrate_companies_key_info_to_jsonb.ts
@@ -0,0 +1,36 @@
+import * as dotenv from 'dotenv';
+dotenv.config({ path: './.env' });
+import { getClient } from './postgres-helpers';
+
+(async () => {
+  const client = await getClient();
+
+  const queryResults = await client.query(
+    'SELECT id, name, layer, layer_detail, ico_start, ico_end, investor_amount, total_valuation, market_verified FROM companies',
+  );
+
+  for (let i = 0; i < queryResults.rows.length; i += 1) {
+    const record = queryResults.rows[i];
+
+    console.log(
+      `Update urls column for data row id ${record.id} name ${record.name}`,
+    );
+
+    const urls = {
+      layer: record.layer,
+      layer_detail: record.layer_detail,
+      ico_start: record.ico_start,
+      ico_end: record.ico_end,
+      investor_amount: record.investor_amount,
+      total_valuation: record.total_valuation,
+      market_verified: record.market_verified,
+    };
+
+    await client.query('UPDATE companies SET key_info = $1 WHERE id = $2', [
+      urls,
+      record.id,
+    ]);
+  }
+
+  await client.end();
+})();
