3217739c0685e16b20eccbae0d9614d7c2e8eba7
raylopro <contact@raylopro.com>
2023-06-21 18:56:37 -0700

    features

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,17 @@
 import type { NextPage } from 'next';
 import Script from 'next/script';
-import React, { useState } from 'react';
+import React, { useState, Fragment, useRef } from 'react';
 import { ElemButton } from '@/components/elem-button';
-import { IconChevronDown, IconCheck } from '@/components/icons';
+import {
+  IconChevronDown,
+  IconCheck,
+  IconChevronDownMini,
+} from '@/components/icons';
 import { useUser } from '@/context/user-context';
-import { FigureCircleDashes, FigureConnect } from '@/components/figures';
-import Image from 'next/image';
+import { FigureConnect } from '@/components/figures';
 import { Tab, Transition } from '@headlessui/react';
 import { Popups } from '@/components/the-navbar';
+import { useIntercom } from 'react-use-intercom';
 
 type Props = {
   setShowPopup: React.Dispatch<React.SetStateAction<Popups>>;
@@ -16,16 +20,118 @@ type Props = {
 const Home: NextPage<Props> = ({ setShowPopup }) => {
   const { user, loading } = useUser();
 
+  const { show } = useIntercom();
+
   const [tabIndex, setTabIndex] = useState(0);
+
+  const logos = [
+    {
+      src: '/images/logos/akkadian.svg',
+      alt: 'Akkadian',
+    },
+    {
+      src: '/images/logos/capital6-eagle.svg',
+      alt: 'Capital6 Eagle',
+    },
+    {
+      src: '/images/logos/ankr.svg',
+      alt: 'Ankr',
+    },
+    {
+      src: '/images/logos/toa-festival.svg',
+      alt: 'TOA Festival',
+    },
+    {
+      src: '/images/logos/defy-trends.svg',
+      alt: 'DeFy Trends',
+    },
+    {
+      src: '/images/logos/4k.svg',
+      alt: '4K',
+    },
+    {
+      src: '/images/logos/wearable-technologies.svg',
+      alt: 'Wearable Technologies',
+      className: 'hidden sm:block',
+    },
+  ];
+
+  const [selectedFeature, setSelectedFeature] = useState(0);
+  const [selectedFeature2, setSelectedFeature2] = useState(0);
+
+  const features = [
+    {
+      title: 'Lists',
+      content:
+        'Create your own lists for competitive research, due diligence, portfolio management and more.',
+      src: '/images/features/lists.png',
+    },
+    {
+      title: 'Filters',
+      content:
+        'Filter by location, projects, team size, investment total, and industry tags.',
+      src: '/images/features/filters.png',
+    },
+    {
+      title: 'Groups',
+      content:
+        'Create Groups with your team, friends or anyone you want to compare notes with, share insights, track leads and more.',
+      src: '/images/features/groups.png',
+    },
+    {
+      title: 'Notes',
+      content:
+        'Make notes on companies and investors you’re following. Choose to keep them private or share them in your groups.',
+      src: '/images/features/notes.png',
+    },
+    {
+      title: 'Search',
+      content:
+        'Advanced search utilizes our intelligent tagging system to interpret user intent and compare it against the tagged attributes of the items in our database.',
+      src: '/images/features/search.png',
+    },
+  ];
+
+  const features2 = [
+    {
+      title: 'Companies',
+      content:
+        'Browse recently discovered projects and trending companies or use advanced filtering to search based on your interests and investment thesis.',
+      src: '/images/features/companies.png',
+    },
+    {
+      title: 'Investors',
+      content:
+        'Search for relevant investors based on tags, find out who is currently deploying capital and react to recent deals or funds.',
+      src: '/images/features/investors.png',
+    },
+    {
+      title: 'Events',
+      content:
+        'Discover and RSVP for the hottest events in Web3 and AI(coming soon).',
+      src: '/images/features/events.png',
+    },
+    {
+      title: 'News',
+      content: 'Get personalized news from our exclusive media partners.',
+      src: '/images/features/news.png',
+    },
+    {
+      title: 'Notifications',
+      content:
+        'Get real-time notifications on companies, funds, investments, events and news.',
+      src: '/images/features/notifications.png',
+    },
+  ];
 
   return (
     <>
-      <section className="relative px-6 lg:px-8 border-b border-slate-300 bg-gradient-to-b from-transparent to-white">
+      <section className="relative px-2 lg:px-6 border-b border-slate-300 bg-gradient-to-b from-transparent to-white">
         <div
-          className="absolute inset-x-0 top-80 -z-10 transform-gpu overflow-hidden blur-3xl"
+          className="absolute inset-x-0 bottom-16 -z-10 transform-gpu overflow-hidden blur-3xl"
           aria-hidden="true"
         >
-          <div className="relative h-[500px] w-full bg-gradient-to-br from-[#F8DA4B] to-[#FE33D0] opacity-20"></div>
+          <div className="relative h-[500px] w-full bg-gradient-to-br from-[#F8DA4B] to-[#FE33D0] opacity-50"></div>
         </div>
         <div className="mx-auto max-w-4xl pt-32 pb-20 sm:pt-36 sm:pb-24">
           <div className="mb-6 flex justify-center">
@@ -67,88 +173,184 @@ const Home: NextPage<Props> = ({ setShowPopup }) => {
         </div>
       </section>
 
-      <section className="mx-auto max-w-7xl px-6 pt-24 pb-20 lg:px-8">
+      <section className="mx-auto max-w-7xl px-2 pt-24 pb-20 lg:px-6">
         <h2 className="mx-auto max-w-lg text-center text-lg text-zinc-500 leading-8 lg:px-4">
           The most innovative companies are already using EdgeIn to find
           superior data and context.
         </h2>
-        <div className="mt-10 grid grid-cols-3 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-3 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-7">
-          <Image
-            src="/images/logos/akkadian.svg"
-            alt="Akkadian"
-            width={158}
-            height={48}
-            loading="lazy"
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-          />
-          <Image
-            src="/images/logos/capital6-eagle.svg"
-            alt="Capital6 Eagle"
-            width={158}
-            height={48}
-            loading="lazy"
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-          />
-          <Image
-            src="/images/logos/ankr.svg"
-            alt="Ankr"
-            width={158}
-            height={48}
-            loading="lazy"
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-          />
-          <Image
-            src="/images/logos/toa-festival.svg"
-            alt="TOA Festival"
-            width={158}
-            height={48}
-            loading="lazy"
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-          />
-          <Image
-            src="/images/logos/defy-trends.svg"
-            alt="DeFy Trends"
-            width={158}
-            height={48}
-            loading="lazy"
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-          />
-          <Image
-            src="/images/logos/4k.svg"
-            alt="4K"
-            width={158}
-            height={48}
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-          />
-          <Image
-            src="/images/logos/wearable-technologies.svg"
-            alt="Wearable Technologies"
-            width={158}
-            height={48}
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-          />
+        <div className="mt-10 grid grid-cols-4 items-center gap-x-8 gap-y-10 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-7">
+          {logos.map((organization: any, index: number) => {
+            const { src, alt, className } = organization;
+            return (
+              <img
+                key={index}
+                src={src}
+                alt={alt}
+                className={`col-span-2 max-h-12 w-full object-contain sm:col-span-1 ${
+                  className ? className : ''
+                }`}
+              />
+            );
+          })}
         </div>
       </section>
 
-      <section className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div>How EdgeIn works</div>
+      <section className="relative">
+        <h2 className="text-3xl text-center text-white py-16 font-bold tracking-tight sm:text-5xl">
+          How EdgeIn works
+        </h2>
+        <div className="absolute top-0 left-0 right-0 h-[28rem] bg-gradient-to-b from-primary-400 via-primary-500 to-primary-500 -z-10"></div>
+
+        <div className="mx-auto max-w-7xl px-2 lg:px-6">
+          <div className="bg-gray-50 rounded-xl p-6 lg:p-14">
+            {/* Features Section 1 */}
+            <div className="md:grid md:grid-cols-12 md:gap-16">
+              <div className="flex flex-col sm:space-y-4 md:col-span-5">
+                {features.map((feature: any, index: number) => {
+                  const { title, content } = feature;
+                  return (
+                    <div
+                      key={index}
+                      className={`px-4 py-3 border-l-[3px] rounded-r-xl ${
+                        selectedFeature === index
+                          ? 'transform border-l-primary-500 bg-white'
+                          : 'border-transparent bg-transparent'
+                      }`}
+                    >
+                      <button
+                        onClick={() => setSelectedFeature(index)}
+                        className={`whitespace-nowrap flex items-center w-full font-bold text-lg transition-all ${
+                          selectedFeature === index ? 'text-primary-500' : ''
+                        }`}
+                      >
+                        {title}
+                        <IconChevronDownMini
+                          className={`h-5 w-5 transition-all ease-in-out duration-100 ${
+                            selectedFeature === index
+                              ? 'transform text-primary-500 rotate-180'
+                              : ''
+                          }`}
+                        />
+                      </button>
+                      <Transition
+                        show={selectedFeature === index}
+                        className="overflow-hidden"
+                        enter="transition-all ease-in-out duration-1000"
+                        enterFrom="transform max-h-0"
+                        enterTo="transform max-h-screen"
+                        leave="transition-all ease-linear duration-75"
+                        leaveFrom="transform max-h-screen"
+                        leaveTo="transform max-h-0"
+                      >
+                        <div className="pt-1">{content}</div>
+                      </Transition>
+                    </div>
+                  );
+                })}
+              </div>
+
+              <div className="mt-4 md:mt-0 md:col-span-7">
+                <div className="relative flex items-center rounded-xl overflow-hidden">
+                  <div className="bg-primary-500/10">
+                    {selectedFeature === 3 && (
+                      <div className="absolute top-0 bottom-0 right-0 w-16 bg-gradient-to-r from-transparent to-[#E5E3F7]"></div>
+                    )}
+
+                    <div className="absolute bottom-0 left-0 right-0 h-28 bg-gradient-to-b from-transparent to-[#E5E3F7]"></div>
+
+                    <img
+                      src={features[selectedFeature].src}
+                      alt={features[selectedFeature].title}
+                      className="w-full"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* Features Section 2 */}
+            <div className="mt-20 md:grid md:grid-cols-12 md:gap-16">
+              <div className="flex flex-col sm:space-y-4 md:col-span-5 md:order-last">
+                {features2.map((feature: any, index: number) => {
+                  const { title, content } = feature;
+                  return (
+                    <div
+                      key={index}
+                      className={`px-4 py-3 border-l-[3px] rounded-r-xl ${
+                        selectedFeature2 === index
+                          ? 'transform border-l-primary-500 bg-white'
+                          : 'border-transparent bg-transparent'
+                      }`}
+                    >
+                      <button
+                        onClick={() => setSelectedFeature2(index)}
+                        className={`whitespace-nowrap flex items-center w-full font-bold text-lg transition-all ${
+                          selectedFeature2 === index ? 'text-primary-500' : ''
+                        }`}
+                      >
+                        {title}
+                        <IconChevronDownMini
+                          className={`h-5 w-5 transition-all ease-in-out duration-100 ${
+                            selectedFeature2 === index
+                              ? 'transform text-primary-500 rotate-180'
+                              : ''
+                          }`}
+                        />
+                      </button>
+                      <Transition
+                        show={selectedFeature2 === index}
+                        className="overflow-hidden"
+                        enter="transition-all ease-in-out duration-1000"
+                        enterFrom="transform max-h-0"
+                        enterTo="transform max-h-screen"
+                        leave="transition-all ease-linear duration-75"
+                        leaveFrom="transform max-h-screen"
+                        leaveTo="transform max-h-0"
+                      >
+                        <div className="pt-1">{content}</div>
+                      </Transition>
+                    </div>
+                  );
+                })}
+              </div>
+
+              <div className="mt-4 md:mt-0 md:col-span-7">
+                <div className="relative flex items-center rounded-xl overflow-hidden">
+                  <div className="bg-primary-500/10">
+                    {/* {selectedFeature2 === 3 && (
+                      <div className="absolute top-0 bottom-0 right-0 w-16 bg-gradient-to-r from-transparent to-[#E5E3F7]"></div>
+                    )} */}
+
+                    <div className="absolute bottom-0 left-0 right-0 h-28 bg-gradient-to-b from-transparent to-[#E5E3F7]"></div>
+
+                    <img
+                      src={features2[selectedFeature2].src}
+                      alt={features2[selectedFeature2].title}
+                      className="w-full"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
 
       <section className="">
-        <div className="mx-auto max-w-2xl text-center pt-24 pb-16 px-6 lg:px-0">
-          <h1 className="text-3xl font-bold tracking-tight sm:text-5xl">
+        <div className="mx-auto max-w-2xl text-center pt-24 pb-16 px-2 lg:px-0">
+          <h2 className="text-3xl font-bold tracking-tight sm:text-5xl">
             Empowering the community with better data.
-          </h1>
-          <p className="mt-6 leading-8 text-zinc-500 sm:text-lg sm:px-8">
+          </h2>
+          <p className="mt-6 leading-8 text-zinc-500 sm:text-lg sm:px-6">
             EdgeIn is the largest on/off-chain database in Web3 and AI, and the
             only community-driven data platform for teams, investors, event
             organizers, and media to gain an edge in trending markets.
           </p>
         </div>
 
-        <div className="mx-auto max-w-7xl">
+        <div className="mx-auto max-w-7xl lg:px-6">
           <Tab.Group selectedIndex={tabIndex} onChange={setTabIndex}>
-            <Tab.List className="scrollbar-hide overflow-x-scroll pb-px flex shrink-0 gap-x-6 px-6 font-bold text-lg border-b border-black/10 transition-all lg:justify-center lg:px-0">
+            <Tab.List className="scrollbar-hide overflow-x-scroll pb-px flex shrink-0 gap-x-6 px-6 font-bold text-lg border-b border-black/10 transition-all lg:justify-center">
               <Tab
                 className={({ selected }) =>
                   `${
@@ -321,19 +523,19 @@ const Home: NextPage<Props> = ({ setShowPopup }) => {
               </li>
             </ul>
 
-            <div className="mt-8 flex justify-center space-x-6">
+            <div className="mt-8 flex flex-col justify-center space-y-6 sm:flex-row sm:items-center sm:space-x-6 sm:space-y-0">
               <ElemButton
                 onClick={() => setShowPopup('signup')}
                 btn="secondary"
                 arrow
-                className="whitespace-nowrap"
+                className="whitespace-nowrap w-fit mx-auto sm:mx-0"
               >
                 Start for free
               </ElemButton>
               <ElemButton
-                onClick={() => setShowPopup('signup')}
+                onClick={show}
                 btn="ol-white"
-                className="whitespace-nowrap"
+                className="whitespace-nowrap w-fit mx-auto sm:mx-0"
               >
                 Talk to us
               </ElemButton>
