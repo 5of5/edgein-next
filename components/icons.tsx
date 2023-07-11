06ebb893e73a04d18cc1907d6ef6acbc30cda174
raylopro <contact@raylopro.com>
2023-07-11 16:15:09 +0200

    repeat icons fix

--- components/icons.tsx
+++ components/icons.tsx
@@ -1470,29 +1470,6 @@ export const IconCompanies: React.FC<IconProps> = ({
   );
 };
 
-export const IconCalendar: React.FC<IconProps> = ({
-  className,
-  title,
-  strokeWidth,
-}) => {
-  return (
-    <svg
-      className={className}
-      fill="none"
-      stroke="currentColor"
-      strokeWidth={strokeWidth ? strokeWidth : 1.5}
-      viewBox="0 0 24 24"
-      aria-hidden="true">
-      <title>{title ? title : 'Calendar'}</title>
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5"
-      />
-    </svg>
-  );
-};
-
 export const IconCalendarDays: React.FC<IconProps> = ({
   className,
   title,
@@ -1507,57 +1484,6 @@ export const IconCalendarDays: React.FC<IconProps> = ({
       viewBox="0 0 24 24"
       aria-hidden="true">
       <title>{title ? title : 'Calendar'}</title>
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5"
-      />
-    </svg>
-  );
-};
-
-export const IconCalendarAdd: React.FC<IconProps> = ({
-  className,
-  title,
-  strokeWidth,
-}) => {
-  return (
-    <svg
-      className={className}
-      fill="none"
-      stroke="currentColor"
-      strokeWidth={strokeWidth ? strokeWidth : 1.5}
-      viewBox="0 0 24 24"
-      aria-hidden="true">
-      <title>{title ? title : 'Add to Calendar'}</title>
-      <path
-        d="M6.75 3V5.25M17.25 3V5.25M3 18.75V7.5C3 6.90326 3.23705 6.33097 3.65901 5.90901C4.08097 5.48705 4.65326 5.25 5.25 5.25H18.75C19.3467 5.25 19.919 5.48705 20.341 5.90901C20.7629 6.33097 21 6.90326 21 7.5V18.75M3 18.75C3 19.3467 3.23705 19.919 3.65901 20.341C4.08097 20.7629 4.65326 21 5.25 21H18.75C19.3467 21 19.919 20.7629 20.341 20.341C20.7629 19.919 21 19.3467 21 18.75M3 18.75V11.25C3 10.6533 3.23705 10.081 3.65901 9.65901C4.08097 9.23705 4.65326 9 5.25 9H18.75C19.3467 9 19.919 9.23705 20.341 9.65901C20.7629 10.081 21 10.6533 21 11.25V18.75"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M12 12V18M15 15H9"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  );
-};
-
-export const IconCalendar: React.FC<IconProps> = ({
-  className,
-  title,
-  strokeWidth,
-}) => {
-  return (
-    <svg
-      className={className}
-      fill="none"
-      stroke="currentColor"
-      strokeWidth={strokeWidth ? strokeWidth : 1.5}
-      viewBox="0 0 24 24"
-      aria-hidden="true">
-      <title>{title ? title : 'Add to Calendar'}</title>
       <path
         strokeLinecap="round"
         strokeLinejoin="round"
