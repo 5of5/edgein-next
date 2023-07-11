d5220a8eced8edffbb41e846b2cac53b7064c83b
Roman Selmeci <selmeci.roman@gmail.com>
2023-07-11 09:39:47 +0200

    fix: wait to finish log in during auth setup

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,4 +1,4 @@
-import { test as setup } from '@playwright/test';
+import { expect, test as setup } from '@playwright/test';
 import { getLoginPayload, loginUser } from './factories/auth';
 
 const URL = 'http://localhost:3000/';
@@ -8,7 +8,14 @@ const file = 'playwright/.auth/user.json';
 setup('Authenticate', async ({ page }) => {
   await page.goto(URL);
 
-  await loginUser(page, getLoginPayload());
+  const loginPayloadData = getLoginPayload();
+  await loginUser(page, loginPayloadData);
+
+  await expect(
+    page
+      .getByAltText('profile')
+      .or(page.getByRole('img', { name: loginPayloadData.name })),
+  ).toBeVisible({ timeout: 15000 });
 
   await page.context().storageState({ path: file });
 });
