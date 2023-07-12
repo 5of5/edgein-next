bdecafac91b55db399b240c8e542f785f66b0a96
Roman Selmeci <selmeci.roman@gmail.com>
2023-07-12 08:43:59 +0200

    fix: slug creation in tests, v4

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI ? 'github' : 'list',
-  timeout: 5000,
+  timeout: 15000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
