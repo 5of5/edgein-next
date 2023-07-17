7927503caa449e1a07689f5ce5f046e51111f171
Roman Selmeci <selmeci.roman@gmail.com>
2023-07-17 17:28:38 +0200

    wip

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,5 +1,3 @@
 export const redirect_url = (): string => {
-  return process.env.VERCEL_ENV !== 'production'
-    ? `https://${process.env.VERCEL_URL}`
-    : process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URL!;
+  return process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URL!;
 };
