7215746108ac56971c503eb8c566b4acc2fae464
Tomas Kucharik <thomaas228@gmail.com>
2023-08-04 19:08:59 +0200

    adds locationtags parsing

--- utils/personalizedTags.ts
+++ utils/personalizedTags.ts
@@ -1,12 +1,24 @@
-import { User } from "@/models/user";
+import { User } from '@/models/user';
 
 export const getPersonalizedData = ({
   user,
 }: {
   user: User | null;
-}): { locationTags: string[]; industryTags: string[] } => ({
-  locationTags:
-    (user?.onboarding_information?.locationTags as Array<string>) ?? [],
-  industryTags:
-    (user?.onboarding_information?.industryTags as Array<string>) ?? [],
-});
+}): { locationTags: string[]; industryTags: string[] } => {
+  const locationTagsRaw =
+    (user?.onboarding_information?.locationTags as Array<string>) ?? [];
+
+  // This jargon is due to inconsistency of location values
+  // in the onboarding_information jsonb column, the location is
+  // saved like "Tel Aviv, TA ISR" so we parse it and return the first part
+  // until ','
+  const locationTags = locationTagsRaw.map(
+    rawLocation => rawLocation.split(',')[0] ?? rawLocation,
+  );
+
+  return {
+    locationTags,
+    industryTags:
+      (user?.onboarding_information?.industryTags as Array<string>) ?? [],
+  };
+};
