830285753320b579eb9db63634e57cfb73853d36
Ed Parsons <ed@acutulus.co>
2023-08-10 14:49:34 -0700

    testing parenthses

--- pages/sitemap.xml.ts
+++ pages/sitemap.xml.ts
@@ -6,28 +6,28 @@ import { GetServerSidePropsContext } from 'next';
 const xml = (rootUrl: string, data?: GetSiteMapAggregatesQuery) => `<?xml version="1.0" encoding="UTF-8"?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <sitemap><loc>${rootUrl}/sitemap-index.xml</loc></sitemap>
-  <sitemap><loc>${Math.ceil(data?.companies_aggregate.aggregate?.count || 1 / PER_PAGE_LIMIT)}/sitemap/companies/0.xml</loc></sitemap>
-  <sitemap><loc>${Math.ceil(data?.events_aggregate.aggregate?.count || 1 / PER_PAGE_LIMIT)}/sitemap/companies/0.xml</loc></sitemap>
-  <sitemap><loc>${Math.ceil(data?.vc_firms_aggregate.aggregate?.count || 1 / PER_PAGE_LIMIT)}/sitemap/companies/0.xml</loc></sitemap>
-  <sitemap><loc>${Math.ceil(data?.people_aggregate.aggregate?.count || 1 / PER_PAGE_LIMIT)}/sitemap/companies/0.xml</loc></sitemap>
+  <sitemap><loc>${Math.ceil((data?.companies_aggregate.aggregate?.count || 1) / PER_PAGE_LIMIT)}/sitemap/companies/0.xml</loc></sitemap>
+  <sitemap><loc>${Math.ceil((data?.events_aggregate.aggregate?.count || 1) / PER_PAGE_LIMIT)}/sitemap/companies/0.xml</loc></sitemap>
+  <sitemap><loc>${Math.ceil((data?.vc_firms_aggregate.aggregate?.count || 1) / PER_PAGE_LIMIT)}/sitemap/companies/0.xml</loc></sitemap>
+  <sitemap><loc>${Math.ceil((data?.people_aggregate.aggregate?.count || 1) / PER_PAGE_LIMIT)}/sitemap/companies/0.xml</loc></sitemap>
   </sitemapindex>`;
 function SiteMap() {
   // getServerSideProps will do the heavy lifting
 }
-// ${ Array.from({length: Math.ceil(data?.companies_aggregate.aggregate?.count || 1 / PER_PAGE_LIMIT)}, (_, i) => 
+// ${ Array.from({length: Math.ceil((data?.companies_aggregate.aggregate?.count || 1) / PER_PAGE_LIMIT)}, (_, i) => 
 // `   <sitemap><loc>${rootUrl}/sitemap/companies/${i}.xml</loc></sitemap>
 // `
 // )}
 
-// ${ Array.from({length: Math.ceil(data?.events_aggregate.aggregate?.count || 1 / PER_PAGE_LIMIT)}, (_, i) => 
+// ${ Array.from({length: Math.ceil((data?.events_aggregate.aggregate?.count || 1) / PER_PAGE_LIMIT)}, (_, i) => 
 // `   <sitemap><loc>${rootUrl}/sitemap/events/${i}.xml</loc></sitemap>
 // `
 // ) }
-// ${ Array.from({length: Math.ceil(data?.vc_firms_aggregate.aggregate?.count || 1 / PER_PAGE_LIMIT)}, (_, i) => 
+// ${ Array.from({length: Math.ceil((data?.vc_firms_aggregate.aggregate?.count || 1) / PER_PAGE_LIMIT)}, (_, i) => 
 // `   <sitemap><loc>${rootUrl}/sitemap/investors/${i}.xml</loc></sitemap>
 // `
 // ) }
-// ${ Array.from({length: Math.ceil(data?.people_aggregate.aggregate?.count || 1 / PER_PAGE_LIMIT)}, (_, i) => 
+// ${ Array.from({length: Math.ceil((data?.people_aggregate.aggregate?.count || 1) / PER_PAGE_LIMIT)}, (_, i) => 
 // `   <sitemap><loc>${rootUrl}/sitemap/people/${i}.xml</loc></sitemap>
 // `
 // ) } 
