5230fb015b7342b9ec4f081510a5c174e2bacf3f
Tomas Kucharik <thomaas228@gmail.com>
2023-08-14 11:33:23 +0200

    ui updates

--- components/invites/elem-invite-investment-members.tsx
+++ components/invites/elem-invite-investment-members.tsx
@@ -1,17 +1,16 @@
 import { useMutation } from 'react-query';
-import { InviteToEdgeInPayload } from '@/types/api';
 import { useUser } from '@/context/user-context';
 import {
   Users,
   Investors,
   useGetInvestorMailingListQuery,
   useGetUserByIdQuery,
+  Companies,
 } from '@/graphql/types';
 import { ElemButton } from '../elem-button';
-import { flatten, get, has, includes, uniqBy } from 'lodash';
+import { filter, flatten, get, has, uniqBy } from 'lodash';
 import { DeepPartial } from '@/types/common';
 import { ElemPhoto } from '../elem-photo';
-import Link from 'next/link';
 import { useState } from 'react';
 
 const getInvestedInCompanies = (investors: DeepPartial<Investors>[]) => {
@@ -45,11 +44,11 @@ type SendInvitationPayload = {
 };
 
 export const ElemInviteInvestmentMembers = () => {
-  const { user } = useUser();
+  const [selectedCompany, setSelectedCompany] = useState<
+    DeepPartial<Companies> | undefined | null
+  >(undefined);
 
-  const [isLoadingButtonIndex, setIsLoadingButtonIndex] = useState<
-    number | undefined
-  >();
+  const { user } = useUser();
 
   const { data: userById, refetch: refetchUser } = useGetUserByIdQuery({
     id: user?.id || 0,
@@ -64,11 +63,16 @@ export const ElemInviteInvestmentMembers = () => {
 
   const companies = getInvestedInCompanies(mailingList.data?.investors ?? []);
 
-  const {
-    data: sendInvitationEmailResponse,
-    mutate: sendInvitationEmailMutation,
-    isLoading,
-  } = useMutation<unknown, unknown, SendInvitationPayload>({
+  const companiesWithTeamMembers = filter(
+    companies,
+    company => (company?.teamMembers?.length ?? []) > 0,
+  );
+
+  const { mutate: sendInvitationEmailMutation, isLoading } = useMutation<
+    unknown,
+    unknown,
+    SendInvitationPayload
+  >({
     mutationFn: ({ companyId }) =>
       fetch('/api/send-invite-to-investment-members/', {
         method: 'POST',
@@ -78,58 +82,98 @@ export const ElemInviteInvestmentMembers = () => {
         },
         body: JSON.stringify({ companyId }),
       }),
-    onSuccess: () => setIsLoadingButtonIndex(undefined),
-    onError: () => setIsLoadingButtonIndex(undefined),
+    onSuccess: () => setSelectedCompany(undefined),
     onSettled: () => refetchUser(),
   });
 
-  const handleSendEmails = async (companyId?: number) => {
-    if (companyId) {
-      setIsLoadingButtonIndex(companyId);
-      sendInvitationEmailMutation({ companyId });
+  const handleClick = (company: DeepPartial<Companies> | undefined | null) => {
+    if (selectedCompany === company) {
+      setSelectedCompany(undefined);
+    } else {
+      setSelectedCompany(company);
+    }
+  };
+
+  const handleSendEmails = async () => {
+    if (selectedCompany?.id) {
+      sendInvitationEmailMutation({
+        companyId: selectedCompany.id,
+      });
     }
   };
 
   return (
     <div className="p-5 bg-white rounded-lg border border-black/10">
-      <div className="flex flex-col lg:gap-1">
-        <h3 className="font-bold">Invite your team</h3>
-        <p className="text-sm text-slate-600">
-          Invite people that are part of the companies you invested in
-        </p>
+      <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2">
+          <h3 className="font-bold">Invite from your portfolio</h3>
+          <p className="text-sm text-slate-600">
+            Select a company from your portfolio to invite their team
+          </p>
+        </div>
 
-        <div className="grid grid-cols-2 gap-4 p-5 bg-white rounded-lg border border-black/10">
-          {companies.map(company => (
-            <div key={company?.id} className="flex flex-row gap-4 items-center">
-              <ElemPhoto
-                photo={company?.logo}
-                wrapClass="flex items-center justify-center shrink-0 w-12 h-12 p-1 rounded-lg overflow-hidden border border-slate-200"
-                imgClass="object-fit max-w-full max-h-full"
-                imgAlt={company?.name}
-                placeholderClass="text-slate-300"
-              />
-
-              <div>
-                <span className="line-clamp-2 font-bold">{company?.name}</span>
-                <span className="line-clamp-2 font-normal text-sm text-slate-600">
-                  {company?.teamMembers?.length} emails
-                </span>
-              </div>
-
-              <ElemButton
-                btn="purple"
-                onClick={() => handleSendEmails(company?.id)}
-                loading={isLoadingButtonIndex === company?.id && isLoading}
-                disabled={
-                  !company?.teamMembers?.length ||
-                  !canSendInvestorInvitation(userById?.users, company?.id) ||
-                  !user
-                }
+        <div className="grid grid-cols-3 gap-4">
+          {companiesWithTeamMembers.map(company =>
+            canSendInvestorInvitation(userById?.users, company?.id) ? (
+              <div
+                key={company?.id}
+                className={`flex flex-row items-center py-2 px-3 rounded-lg border border-slate-200 hover:bg-slate-50 ${selectedCompany === company ? 'border-primary-400' : ''
+                  }`}
+                onClick={() => handleClick(company)}
               >
-                Invite
-              </ElemButton>
+                <ElemPhoto
+                  photo={company?.logo}
+                  wrapClass="flex items-center justify-center shrink-0 w-12 h-12 p-1 rounded-lg overflow-hidden border border-slate-200"
+                  imgClass="object-fit max-w-full max-h-full"
+                  imgAlt={company?.name}
+                  placeholderClass="text-slate-300"
+                />
+
+                <div className="flex flex-col ml-2">
+                  <div className="font-semibold line-clamp-2">
+                    {company?.name}
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div
+                key={company?.id}
+                className={`flex flex-row justify-start items-center py-2 px-3 rounded-lg border border-slate-200`}
+              >
+                <ElemPhoto
+                  photo={company?.logo}
+                  wrapClass="flex items-center justify-center shrink-0 w-12 h-12 p-1 rounded-lg overflow-hidden border border-slate-200"
+                  imgClass="object-fit max-w-full max-h-full opacity-50"
+                  imgAlt={company?.name}
+                  placeholderClass="text-slate-300"
+                />
+
+                <div className="flex flex-col ml-2 justify-center text-slate-400">
+                  <div className="font-semibold line-clamp-2">
+                    {company?.name}
+                  </div>
+                  <div className="text-xs">Sent</div>
+                </div>
+              </div>
+            ),
+          )}
+        </div>
+
+        <div className="flex flex-row gap-4 mt-2">
+          <ElemButton
+            btn="purple"
+            onClick={handleSendEmails}
+            loading={isLoading}
+            disabled={!user || selectedCompany === undefined}
+          >
+            Invite
+          </ElemButton>
+
+          {selectedCompany !== undefined && (
+            <div className="text-sm text-slate-400 my-auto">
+              {selectedCompany?.teamMembers?.length} people will be notified
             </div>
-          ))}
+          )}
         </div>
       </div>
     </div>
