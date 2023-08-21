0ec681336c5cd8e1d02edcd5a5096bd395ae38b9
Roman <romanselmeci@sudolabs.io>
2023-08-21 12:11:10 +0200

    fix(google): Check IP for Googlebot, only if presented in headers (#703)

--- utils/googlebot-verify.ts
+++ utils/googlebot-verify.ts
@@ -5,17 +5,17 @@ import userTriggerFetchers from './googlebotjson/user-triggered-fetchers.json';
 import { matches } from 'ip-matching';
 
 type Ipv4Prefix = {
-    ipv4Prefix: string;
-}
+  ipv4Prefix: string;
+};
 
 type Ipv6Prefix = {
-    ipv6Prefix: string;
-}
+  ipv6Prefix: string;
+};
 
 type Googlebot = {
-    creationTime: string;
-    prefixes: (Ipv4Prefix | Ipv6Prefix)[]
-}
+  creationTime: string;
+  prefixes: (Ipv4Prefix | Ipv6Prefix)[];
+};
 
 // Google recommends using a reverse dn lookup
 // this is a library that implments this npm i googlebot-verify
@@ -23,12 +23,27 @@ type Googlebot = {
 // edge environments do not have access to node dns module
 // so the back up is to implement an ip lookup based on the prefixes provided by google
 
-export const verify = (ip: string): boolean => {
-    const specialCrawlersPrefixes = (specialCrawlers as Googlebot).prefixes;
-    const googlebotPrefixes = (googlebot as Googlebot).prefixes;
-    const userTriggerFetchersPrefixes = (userTriggerFetchers as Googlebot).prefixes;
-    const allPrefixes = [...specialCrawlersPrefixes, ...googlebotPrefixes, ...userTriggerFetchersPrefixes];
-    const ipv4Prefixes = allPrefixes.filter((prefix) => 'ipv4Prefix' in prefix).map((prefix) => (prefix as Ipv4Prefix).ipv4Prefix);
-    const ipv6Prefixes = allPrefixes.filter((prefix) => 'ipv6Prefix' in prefix).map((prefix) => (prefix as Ipv6Prefix).ipv6Prefix);
-    return ipv4Prefixes.some(ipv4 => matches(ip, ipv4)) || ipv6Prefixes.some(ipv6 => matches(ip, ipv6));
-}
+export const verify = (ip?: string): boolean => {
+  if (!ip) {
+    return false;
+  }
+  const specialCrawlersPrefixes = (specialCrawlers as Googlebot).prefixes;
+  const googlebotPrefixes = (googlebot as Googlebot).prefixes;
+  const userTriggerFetchersPrefixes = (userTriggerFetchers as Googlebot)
+    .prefixes;
+  const allPrefixes = [
+    ...specialCrawlersPrefixes,
+    ...googlebotPrefixes,
+    ...userTriggerFetchersPrefixes,
+  ];
+  const ipv4Prefixes = allPrefixes
+    .filter(prefix => 'ipv4Prefix' in prefix)
+    .map(prefix => (prefix as Ipv4Prefix).ipv4Prefix);
+  const ipv6Prefixes = allPrefixes
+    .filter(prefix => 'ipv6Prefix' in prefix)
+    .map(prefix => (prefix as Ipv6Prefix).ipv6Prefix);
+  return (
+    ipv4Prefixes.some(ipv4 => matches(ip, ipv4)) ||
+    ipv6Prefixes.some(ipv6 => matches(ip, ipv6))
+  );
+};
