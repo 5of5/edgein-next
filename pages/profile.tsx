4dfde78f868778321eee192a7a1ec599d2c147fd
Roman Selmeci <selmeci.roman@gmail.com>
2023-08-22 10:19:41 +0200

    feat(profile): Extend user profile page with new daily emails switcher

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -29,8 +29,10 @@ import { usePopup } from '@/context/popup-context';
 // import { InputDate } from "@/components/InputDate";
 // import { GetStaticProps } from "next";
 import { EditSection } from '@/components/dashboard/edit-section';
+import { ProfileEditDailyEmails } from '@/components/profile/profile-edit-daily-emails';
 // import { functionChoicesTM } from "@/utils/constants";
 // import { ElemCompaniesSearchInput } from "@/components/Companies/ElemCompaniesSearchInput";
+import CookieService from '@/utils/cookie';
 
 const emptyTeamMember = {
   start_date: null,
@@ -526,8 +528,7 @@ const Profile: FC<Props> = ({ companiesDropdown }) => {
               href={`/people/${users?.users_by_pk?.person?.slug}/`}
               btn="purple"
               arrow
-              className="mt-2 sm:mt-0"
-            >
+              className="mt-2 sm:mt-0">
               View Profile
             </ElemButton>
           )}
@@ -549,8 +550,7 @@ const Profile: FC<Props> = ({ companiesDropdown }) => {
                 <ElemButton
                   btn="primary-light"
                   onClick={() => setShowPopup('search')}
-                  className="mt-4"
-                >
+                  className="mt-4">
                   <IconSearch className="h-5 w-5 mr-1.5" aria-hidden="true" />
                   Search your name
                 </ElemButton>
@@ -602,6 +602,7 @@ const Profile: FC<Props> = ({ companiesDropdown }) => {
         ) : (
           <>
             <ProfileEditName />
+            <ProfileEditDailyEmails user={users!.users_by_pk} />
             <ProfileEditEmail />
 
             {users?.users_by_pk?.person && (
