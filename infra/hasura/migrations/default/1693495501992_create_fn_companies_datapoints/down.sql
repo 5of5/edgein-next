7d2c4332e8ce14f891ea2600fbdef15449ea6426
Roman Selmeci <selmeci.roman@gmail.com>
2023-08-31 17:33:07 +0200

    feat: Added computed field datapoints_count to companies table

---
+++ infra/hasura/migrations/default/1693495501992_create_fn_companies_datapoints/down.sql
@@ -0,0 +1,18 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- CREATE OR REPLACE FUNCTION public.companies_datapoints(company_row companies)
+--  RETURNS integer
+--  LANGUAGE plpgsql
+--  STABLE
+-- AS $function$
+-- DECLARE
+--     col_count integer;
+--     members_count integer;
+--     investment_rounds_count integer;
+-- BEGIN
+--      col_count := (SELECT count(value)::int FROM json_each_text(row_to_json(company_row)));
+--      members_count := (SELECT count(*) FROM team_members tm WHERE company_row.id = tm.company_id);
+--      investment_rounds_count = (SELECT count(*) FROM investment_rounds ir WHERE company_row.id = ir.company_id);
+--      RETURN col_count + members_count + investment_rounds_count;
+-- END;
+-- $function$;
