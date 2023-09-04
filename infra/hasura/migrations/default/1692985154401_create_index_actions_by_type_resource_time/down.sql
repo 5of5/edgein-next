5279eb0c5114c273b122c188fe8cff2d0bdefc5b
Roman Selmeci <selmeci.roman@gmail.com>
2023-09-04 08:56:32 +0200

    remove index on the action table

--- infra/hasura/migrations/default/1692985154401_create_index_actions_by_type_resource_time/down.sql
+++
@@ -1,1 +0,0 @@
-DROP INDEX IF EXISTS "public"."actions_by_type_resource_time";
--- infra/hasura/migrations/default/1692985154401_create_index_actions_by_type_resource_time/up.sql
+++
@@ -1,2 +0,0 @@
-CREATE  INDEX "actions_by_type_resource_time" on
-  "public"."actions" using btree ("action", "created_at", "resource", "resource_id");
