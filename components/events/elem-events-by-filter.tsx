475315bd620acec2f6e22da471d043ccdf5e9e27
sudo-ds <davidsvitana@sudolabs.io>
2023-09-05 14:55:18 +0200

    feat: Add groupBy prop to elem-events-by-filter component

--- components/events/elem-events-by-filter.tsx
+++ components/events/elem-events-by-filter.tsx
@@ -1,6 +1,7 @@
 import {
   Events,
   Events_Bool_Exp,
+  Events_Order_By,
   Order_By,
   useGetEventsQuery,
 } from '@/graphql/types';
@@ -16,6 +17,7 @@ import { PlaceholderEventCard } from '../placeholders';
 type Props = {
   headingText: string;
   filters: DeepPartial<Events_Bool_Exp>;
+  orderBy?: DeepPartial<Events_Order_By>;
   itemsPerPage: number;
   tagOnClick: any;
 };
@@ -25,6 +27,7 @@ export const EventsByFilter: FC<Props> = ({
   filters,
   itemsPerPage,
   tagOnClick,
+  orderBy
 }) => {
   const { page, setPage, nextPage, previousPage } = usePagination();
 
@@ -33,7 +36,7 @@ export const EventsByFilter: FC<Props> = ({
       offset: page * itemsPerPage,
       limit: itemsPerPage,
       // @ts-expect-error this should work
-      orderBy: [{ updated_at: Order_By.Desc }],
+      orderBy: [orderBy ?? { updated_at: Order_By.Desc }],
       where: filters as Events_Bool_Exp,
     },
     { refetchOnWindowFocus: false },
@@ -41,7 +44,7 @@ export const EventsByFilter: FC<Props> = ({
 
   if (isLoading) {
     return (
-      <div className="grid gap-8 gap-x-16 grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 mb-16">
+      <div className="grid gap-8 gap-x-16 grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
         {times(4, index => (
           <PlaceholderEventCard key={index} />
         ))}
@@ -65,7 +68,7 @@ export const EventsByFilter: FC<Props> = ({
       <div className="text-4xl font-medium my-4">{headingText}</div>
       <div
         data-testid="personalizedCompanies"
-        className="grid gap-8 gap-x-16 grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 mb-16"
+        className="grid gap-8 gap-x-16 grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4"
       >
         {events.map(event => (
           <ElemEventCard key={event.id} event={event as Events} />
