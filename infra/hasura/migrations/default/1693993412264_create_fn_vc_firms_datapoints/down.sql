33e8e671589041422549efb1fbe0ba4610a90da0
Dávid Svitana <121792229+sudo-ds@users.noreply.github.com>
2023-09-06 12:21:35 +0200

    feat: Add new computed field datapoints_count for vc_firms table (#755)
    * feat: Add new computed field datapoints_count for vc_firms table
    
    * fix: Permissions for datapoints_count in vc_firms table

---
+++ infra/hasura/migrations/default/1693993412264_create_fn_vc_firms_datapoints/down.sql
@@ -0,0 +1,18 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- CREATE OR REPLACE FUNCTION public.vc_firms_datapoints(vc_firm_row vc_firms)
+--  RETURNS integer
+--  LANGUAGE plpgsql
+--  STABLE
+-- AS $function$
+-- DECLARE
+--     col_count integer;
+--     investors_count integer;
+--     investments_count integer;
+-- BEGIN
+--      col_count := (SELECT count(value)::int FROM json_each_text(row_to_json(vc_firm_row)));
+--      investors_count := ((SELECT count(*) FROM investors inv WHERE vc_firm_row.id = inv.vc_firm_id) * 2);
+--      investments_count = ((SELECT count(*) FROM investments invst WHERE vc_firm_row.id = invst.vc_firm_id) * 3);
+--      RETURN col_count + investors_count + investments_count;
+-- END;
+-- $function$;
