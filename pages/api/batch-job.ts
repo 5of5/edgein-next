78000941c4582c7f2b6bf7b6a1ea625cdc2062aa
sudo-ds <davidsvitana@sudolabs.io>
2023-09-29 13:37:44 +0200

    feat: Minor changes from PR review

--- pages/api/batch-job.ts
+++ pages/api/batch-job.ts
@@ -103,6 +103,7 @@ const handleUserTransactions = async (client: Client) => {
   }
 };
 
+//TODO(David): add authentication !
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== 'GET') {
     return res.status(405).json({ message: 'Method not allowed' });
