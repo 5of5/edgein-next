76dcfd88c2b986df15eabdc240c4000cf0bd97e5
Saqirmdevx <martin.nascak@student.tuke.sk>
2023-10-05 16:58:17 +0200

    Update down migration to remove triggers and function

--- infra/hasura/migrations/default/1696511849372_move_people_computed_colums_to_separate_table/down.sql
+++ infra/hasura/migrations/default/1696511849372_move_people_computed_colums_to_separate_table/down.sql
@@ -11,4 +11,8 @@ alter table "public"."people" add column "tags" jsonb;
 alter table "public"."people" alter column "title" drop not null;
 alter table "public"."people" add column "title" text;
 
+DROP TRIGGER IF EXISTS compute_people_data ON team_members;
+DROP TRIGGER IF EXISTS compute_people_data ON investors;
+DROP FUNCTION IF EXISTS public.compute_people_data();
+
 DROP TABLE "public"."people_computed_data";
