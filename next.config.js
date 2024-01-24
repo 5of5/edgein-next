468f0c7cbfcc9389d6348766ed4967ff6a76e667
raylopro <contact@raylopro.com>
2024-01-24 09:19:51 -0800

    login redirect

--- next.config.js
+++ next.config.js
@@ -5,5 +5,14 @@ const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
   ...withImages(),
+  async redirects() {
+    return [
+      {
+        source: '/login',
+        destination: '/sign-in',
+        permanent: true,
+      },
+    ]
+  },
 };
 module.exports = nextConfig;
