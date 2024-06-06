208e0ad7979748cdbf8068cd1c7654a6365feca1
raylopro <contact@raylopro.com>
2024-06-05 22:49:50 -0600

    sidebar list menu search/tooltip/show more

--- components/dashboard/elem-my-lists-menu.tsx
+++ components/dashboard/elem-my-lists-menu.tsx
@@ -1,18 +1,21 @@
-import { getNameFromListName } from '@/utils/reaction';
-import kebabCase from 'lodash/kebabCase';
+import { startCase, kebabCase } from 'lodash';
 import { useRouter } from 'next/router';
-import { FC, useState } from 'react';
-import { IconSidebarList } from '@/components/icons';
+import { FC, SetStateAction, useEffect, useState } from 'react';
+import { IconGlobe, IconLockClosed, IconSidebarList } from '@/components/icons';
 import { useUser } from '@/context/user-context';
 import { ElemUpgradeDialog } from '../elem-upgrade-dialog';
 import { CreateListDialog } from '../my-list/create-list-dialog';
-import { SIDEBAR_DEFAULT_LISTS_LIMIT } from '@/utils/constants';
-import { getListDisplayName } from '@/utils/lists';
 import { ElemWithSignInModal } from '../elem-with-sign-in-modal';
 import { ElemSidebarItem } from './elem-sidebar-item';
+import { InputText } from '@/components/input-text';
 import { ROUTES } from '@/routes';
 import { ElemLink } from '../elem-link';
+import { ElemButton } from '../elem-button';
+import { SIDEBAR_DEFAULT_LISTS_LIMIT } from '@/utils/constants';
+import { getNameFromListName, getListDisplayName } from '@/utils/lists';
+import { formatDateShown } from '@/utils/numbers';
 import { useSidebar } from '@/context/sidebar-context';
+import { ElemTooltip } from '../elem-tooltip';
 
 type Props = {
   className?: string;
@@ -29,9 +32,20 @@ const ElemMyListsMenu: FC<Props> = ({ className = '' }) => {
       : 'text-gray-500';
   };
 
-  const getCustomLists = lists?.filter(
-    list => !['hot', 'crap', 'like'].includes(getNameFromListName(list)),
+  const [customLists, setCustomLists] = useState(
+    lists?.filter(
+      list => !['hot', 'crap', 'like'].includes(getNameFromListName(list)),
+    ),
   );
+  const [limitLists, setLimitLists] = useState(true);
+
+  useEffect(() => {
+    setCustomLists(
+      lists.filter(
+        list => !['hot', 'crap', 'like'].includes(getNameFromListName(list)),
+      ),
+    );
+  }, [lists]);
 
   const [openCreateList, setOpenCreateList] = useState(false);
   const [isOpenUpgradeDialog, setIsOpenUpgradeDialog] = useState(false);
@@ -50,10 +64,14 @@ const ElemMyListsMenu: FC<Props> = ({ className = '' }) => {
     setIsOpenUpgradeDialog(false);
   };
 
+  const onShowMoreLists = () => {
+    setLimitLists(false);
+  };
+
   const onClickCreate = () => {
     if (
       user?.entitlements?.listsCount &&
-      getCustomLists.length > user.entitlements.listsCount
+      customLists.length > user.entitlements.listsCount
     ) {
       return onOpenUpgradeDialog();
     }
@@ -61,15 +79,60 @@ const ElemMyListsMenu: FC<Props> = ({ className = '' }) => {
     return onOpenCreateListDialog();
   };
 
+  const [searchText, setSearchText] = useState('');
+  const [filteredLists, setFilteredLists] = useState(customLists);
+
+  const handleSearchTextChange = (e: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setSearchText(e.target.value);
+  };
+
+  useEffect(() => {
+    if (searchText) {
+      setFilteredLists(
+        customLists.filter(list => {
+          return getNameFromListName(list).toLowerCase().includes(searchText);
+        }),
+      );
+      setLimitLists(false);
+    } else {
+      setFilteredLists(customLists);
+      setLimitLists(true);
+    }
+  }, [searchText, customLists]);
+
   return (
     <li className={className}>
       {user ? (
-        <ElemSidebarItem
-          IconComponent={IconSidebarList}
-          text="Lists"
-          url={ROUTES.LISTS}
-          onClick={() => setShowSidebar(false)}
-        />
+        // <div className="relative">
+        //   <ElemSidebarItem
+        //     IconComponent={IconSidebarList}
+        //     text="Lists"
+        //     url={ROUTES.LISTS}
+        //     onClick={() => setShowSidebar(false)}
+        //   />
+        //   <div className="absolute right-2 top-1.5 bottom-1.5">
+        //     <ElemButton btn="primary" size="xs" onClick={onClickCreate}>
+        //       Create
+        //     </ElemButton>
+        //   </div>
+        // </div>
+        <div className="flex items-center space-x-2">
+          <div className="flex-1">
+            <ElemSidebarItem
+              IconComponent={IconSidebarList}
+              text="Lists"
+              url={ROUTES.LISTS}
+              onClick={() => setShowSidebar(false)}
+            />
+          </div>
+          <div className="">
+            <ElemButton btn="primary" size="xs" onClick={onClickCreate}>
+              Create
+            </ElemButton>
+          </div>
+        </div>
       ) : (
         <ElemWithSignInModal
           wrapperClass="w-full"
@@ -91,46 +154,125 @@ const ElemMyListsMenu: FC<Props> = ({ className = '' }) => {
       )}
 
       {user && (
-        <ul className="mt-1 space-y-1">
-          {lists.slice(0, SIDEBAR_DEFAULT_LISTS_LIMIT).map(listItem => {
-            const listItemId = listItem.id;
-            const listItemName = getNameFromListName(listItem);
+        <>
+          <InputText
+            type="text"
+            onChange={handleSearchTextChange}
+            value={searchText}
+            name="name"
+            autoComplete="off"
+            placeholder="Find a list..."
+            className="mt-2 ring-1 ring-gray-200"
+          />
 
-            return (
+          <ul className="mt-1 space-y-1">
+            {filteredLists
+              ?.slice(
+                0,
+                limitLists ? SIDEBAR_DEFAULT_LISTS_LIMIT : customLists.length,
+              )
+              .map(list => {
+                const listName = getNameFromListName(list);
+
+                const listTooltip = (
+                  <div className="flex-col p-2 group">
+                    <div className="flex items-center gap-x-2">
+                      {list.public ? (
+                        <IconGlobe className="inline-block w-4 h-4 shrink-0" />
+                      ) : (
+                        <IconLockClosed className="inline-block w-4 h-4 shrink-0" />
+                      )}
+
+                      <ElemLink
+                        href={`${ROUTES.LISTS}/${list.id}/${
+                          listName === 'crap' ? 'sh**' : kebabCase(listName)
+                        }`}
+                        className="font-medium leading-snug text-gray-900 line-clamp-2 hover:underline">
+                        {listName}
+                      </ElemLink>
+                      <div className="px-3 py-1 text-xs border border-gray-200 rounded-full">
+                        {list.public ? 'Public' : 'Private'}
+                      </div>
+                    </div>
+                    {list.description && (
+                      <div className="mt-3 text-sm text-gray-500 line-clamp-4">
+                        {list.description}
+                      </div>
+                    )}
+
+                    <div className="flex mt-3 space-x-6 text-xs">
+                      <div className="capitalize">
+                        {list?.created_by?.person ? (
+                          <>
+                            By{' '}
+                            <ElemLink
+                              href={`${ROUTES.PEOPLE}/${list?.created_by?.person?.slug}`}
+                              className="hover:underline">
+                              {list?.created_by?.person.name}
+                            </ElemLink>
+                          </>
+                        ) : (
+                          <>
+                            By{' '}
+                            {startCase(
+                              list?.created_by?.display_name
+                                ? list?.created_by.display_name
+                                : '',
+                            )}
+                          </>
+                        )}
+                      </div>
+                      <div>Updated {formatDateShown(list.updated_at)}</div>
+                    </div>
+                  </div>
+                );
+
+                return (
+                  <li
+                    key={list.id}
+                    role="button"
+                    onClick={() => setShowSidebar(false)}>
+                    <ElemTooltip
+                      content={listTooltip}
+                      direction="top-start"
+                      mode="light"
+                      size="lg"
+                      arrow={false}>
+                      <div>
+                        <ElemLink
+                          href={`${ROUTES.LISTS}/${list.id}/${
+                            listName === 'crap' ? 'sh**' : kebabCase(listName)
+                          }`}
+                          className={`flex items-center space-x-2 py-2 pl-4 font-medium text-sm rounded-md flex-1 transition-all hover:bg-gray-100 hover:text-gray-900 ${getActiveClass(
+                            list.id,
+                            listName === 'crap' ? 'sh**' : kebabCase(listName),
+                          )} `}>
+                          <span className="flex-1 break-all line-clamp-1">
+                            {getListDisplayName(list)}
+                          </span>
+                        </ElemLink>
+                      </div>
+                    </ElemTooltip>
+                  </li>
+                );
+              })}
+
+            {limitLists && (
               <li
-                key={listItem.id}
                 role="button"
-                onClick={() => setShowSidebar(false)}>
-                <ElemLink
-                  href={`${ROUTES.LISTS}/${listItemId}/${
-                    listItemName === 'crap' ? 'sh**' : kebabCase(listItemName)
-                  }`}
-                  className={`flex items-center space-x-2 py-2 pl-4 font-medium text-sm rounded-md flex-1 transition-all hover:bg-gray-100 hover:text-gray-900 ${getActiveClass(
-                    listItemId,
-                    listItemName === 'crap' ? 'sh**' : kebabCase(listItemName),
-                  )} `}>
-                  <span className="flex-1 break-all line-clamp-1">
-                    {getListDisplayName(listItem)}
-                  </span>
-                </ElemLink>
+                onClick={onShowMoreLists}
+                className="flex items-center flex-1 py-2 pl-4 text-xs font-normal text-gray-500 transition-all hover:text-primary-500">
+                Show more
               </li>
-            );
-          })}
-
-          <li
-            role="button"
-            onClick={onClickCreate}
-            className="flex items-center flex-1 py-2 pl-4 mt-1 space-x-2 text-sm font-normal text-gray-500 transition-all rounded-md hover:bg-gray-100 hover:text-gray-900">
-            Add a new list
-          </li>
-        </ul>
+            )}
+          </ul>
+        </>
       )}
 
       <CreateListDialog
         isOpen={openCreateList}
         onClose={onCloseCreateListDialog}
       />
-
       <ElemUpgradeDialog
         isOpen={isOpenUpgradeDialog}
         onClose={onCloseUpgradeDialog}
--- components/elem-tooltip.tsx
+++ components/elem-tooltip.tsx
@@ -42,9 +42,9 @@ export const ElemTooltip: FC<PropsWithChildren<Props>> = ({
     modeClasses = '!bg-gray-900/90 !text-gray-300';
     modeArrowClasses = 'before:!text-gray-900/90';
   } else {
-    modeClasses = '!bg-gray-50/90 !text-gray-600 !border !border-gray-200';
+    modeClasses = '!bg-gray-50 !text-gray-600 !border !border-gray-200';
     modeArrowClasses =
-      'before:!text-gray-50/90 before:!border before:!border-gray-200';
+      'before:!text-gray-50 before:!border before:!border-gray-200';
   }
 
   // Sizes
