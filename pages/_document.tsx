8353ed8520675b5709d449d9158a8bc20dbbceec
Jashan Shetty <43118727+eth-jashan@users.noreply.github.com>
2024-12-23 23:57:45 +0530

    Hypelab ads (#996)
    * hypelab ads
    
    wip
    
    * Ads.txt

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,8 +10,24 @@ export default function Document() {
           href="https://fonts.googleapis.com/css?family=Inter:400,500,600&lang=en&display=swap"
           rel="stylesheet"
         />
-        <script async src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}`}
-     crossOrigin="anonymous"></script>
+        {/* HypeLab SDK Script */}
+        <script
+          suppressHydrationWarning={true}
+          dangerouslySetInnerHTML={{
+            __html: `!(function (h, y, p, e, l, a, b) {
+    ((l = document.createElement(h)).async = !0),
+      (l.src = y),
+      (l.onload = function () {
+        (a = { environment: p, propertySlug: e, ...a }), HypeLab.initialize(a);
+      }),
+      (b = document.getElementsByTagName(h)[0]).parentNode.insertBefore(l, b);
+  })('script', 'https://api.hypelab.com/v1/scripts/hp-sdk.js?v=0', 'development', '752f3a51b9', { privacy: { disable_wallet_detection: true } });`,
+          }}
+        />
+        <script
+          async
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}`}
+          crossOrigin="anonymous"></script>
       </Head>
       <body className="bg-black text-gray-300">
         <Main />
